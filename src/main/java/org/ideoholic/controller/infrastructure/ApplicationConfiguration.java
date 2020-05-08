cbc2ffda71f0b5a77a027b1d08810b06baa2af4c
Reehan <reehan.lalkhan@gmail.com>
2020-05-08 17:47:36 +0530

    Adding ApplicationConfiguration and appContext.xml file to load spring configurations from file

---
+++ src/main/java/org/ideoholic/controller/infrastructure/ApplicationConfiguration.java
@@ -0,0 +1,15 @@
+package org.ideoholic.controller.infrastructure;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@Import({ WebXmlConfiguration.class})
+@ImportResource({ "classpath*:appContext.xml" })
+// @PropertySource(value="classpath:META-INF/spring/jdbc.properties")
+@EnableAutoConfiguration
+public class ApplicationConfiguration {
+
+}
