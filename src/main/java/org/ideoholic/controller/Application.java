f628d1c186650c7bf90c294bd36a1ff4ded8c9de
Reehan <reehan.lalkhan@gmail.com>
2020-05-08 06:51:05 +0530

    Moving Spring application initializer to org.ideoholic package

--- src/main/java/org/ideoholic/controller/Application.java
+++ src/main/java/org/ideoholic/controller/Application.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.controller;
+package org.ideoholic.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
