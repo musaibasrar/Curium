9b28681e8721d2723edff5d1b1f70b26936e0781
Reehan <reehan.lalkhan@gmail.com>
2021-09-27 11:04:32 +0530

    Adding starter class and config needed for Springboot application

---
+++ src/test/java/org/ideoholic/curium/CuriumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ideoholic.curium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuriumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
