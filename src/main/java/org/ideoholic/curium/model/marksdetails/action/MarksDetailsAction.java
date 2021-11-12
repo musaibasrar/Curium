36bd9e14b7dbaaac50e5ede54f51e5ca5b60601d
Adeeba09 <63203707+Adeeba09@users.noreply.github.com>
2021-11-12 17:24:45 +0530

    Converted 'MarksDetailsProcess' to controller. (#37)

--- src/main/java/org/ideoholic/curium/model/marksdetails/action/MarksDetailsAction.java
+++ src/main/java/org/ideoholic/curium/model/marksdetails/action/MarksDetailsAction.java
@@ -7,145 +7,129 @@ import org.ideoholic.curium.model.examdetails.service.ExamDetailsService;
 import org.ideoholic.curium.model.marksdetails.service.MarksDetailsService;
 import org.ideoholic.curium.model.std.service.StandardService;
 import org.ideoholic.curium.model.student.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/MarksDetailsProcess")
 
 public class MarksDetailsAction {
 
+	@Autowired
 	HttpServletRequest request;
+	@Autowired
 	HttpServletResponse response;
-	String url;
-	
-	public MarksDetailsAction(HttpServletRequest request, HttpServletResponse response) {
-		this.request = request;
-		this.response = response;
-	}
-
-	public String execute(String action) {
-		if (action.equalsIgnoreCase("addMarks")) {
-			url = addMarks();
-    	}else if (action.equalsIgnoreCase("search")) {
-			url = search();
-		}else if (action.equalsIgnoreCase("viewMarks")) {
-			url = viewMarks();
-		}else if (action.equalsIgnoreCase("getSubjectsExams")) {
-			url = getSubjectsExams();
-		}else if (action.equalsIgnoreCase("updateMarks")) {
-			url = updateMarks();
-		}else if (action.equalsIgnoreCase("deleteMultiple")) {
-			url = deleteMultiple();
-		}else if (action.equalsIgnoreCase("generateReport")) {
-			url = generateReport();
-		}else if (action.equalsIgnoreCase("searchForReport")) {
-			url = searchForReport();
-		}else if (action.equalsIgnoreCase("getGraphicalReportData")) {
-			url = getGraphicalReportData();
-		}else if (action.equalsIgnoreCase("searchForGraphicalReport")) {
-			url = searchForGraphicalReport();
-		}else if (action.equalsIgnoreCase("getStudentGraph")) {
-			url = getStudentGraph();
-		}else if (action.equalsIgnoreCase("progressReport")) {
-			url = progressreport();
-		}else if (action.equalsIgnoreCase("downloadReportCard")) {
-			url = downloadReportCard();
-		}else if (action.equalsIgnoreCase("marksEntry")) {
-			url = marksEntry();
-		}
-
-		return url;
-	}
 
 	
 
-	private String marksEntry() {
+	@GetMapping("/marksEntry")
+	public String marksEntry() {
 		new StandardService(request, response).viewClasses();
-        return "marksentry.jsp";
+        return "marksentry";
 	}
 
-	private String downloadReportCard() {
+	@PostMapping("/downloadReportCard")
+	public String downloadReportCard() {
 		new MarksDetailsService(request, response).downloadReportCard();
-            return "reportcardsaved.jsp";
+            return "reportcardsaved";
 		
 	}
 
-	private String progressreport() {
+	@GetMapping("/progressReport")
+	public String progressreport() {
 		new StandardService(request, response).viewClasses();
-		return "progressreport.jsp";
+		return "progressreport";
 	}
 
-	private String getStudentGraph() {
+	@PostMapping("/getStudentGraph")
+	public String getStudentGraph() {
 		new MarksDetailsService(request, response).getStudentGraph();
-		return "studentgraph.jsp";
+		return "studentgraph";
 	}
 
-	private String getGraphicalReportData() {
+	@GetMapping("/getGraphicalReportData")
+	public String getGraphicalReportData() {
 		new MarksDetailsService(request, response).getStudentList();
 		new StandardService(request, response).viewClasses();
-		return "graphicalreport.jsp";
+		return "graphicalreport";
 	}
 
-	private String searchForReport() {
+	@PostMapping("/searchForReport")
+	public String searchForReport() {
 		new MarksDetailsService(request, response).Search();
-        return "progressreport.jsp";
+        return "progressreport";
 	}
 
-	private String generateReport() {
+	@PostMapping("/generateReport")
+	public String generateReport() {
 		if (new MarksDetailsService(request, response).generateReport()) {
-            return "reportcardsaved.jsp";
+            return "reportcardsaved";
         } else {
-            return "error.jsp";
+            return "error";
         }
 	}
 
-	private String deleteMultiple() {
+	@PostMapping("/deleteMultiple")
+	public String deleteMultiple() {
 		if (new MarksDetailsService(request, response).deleteMultiple()) {
-            return "markssaved.jsp";
+            return "markssaved";
         } else {
-            return "notSaved.jsp";
+            return "notSaved";
         }
 	}
 
-	private String updateMarks() {
+	@PostMapping("/updateMarks")
+	public String updateMarks() {
 		if (new MarksDetailsService(request, response).updateMarks()) {
-            return "markssaved.jsp";
+            return "markssaved";
         } else {
-            return "error.jsp";
+            return "error";
         }
 	}
 
-	private String getSubjectsExams() {
+	@GetMapping("/getSubjectsExams")
+	public String getSubjectsExams() {
 		new MarksDetailsService(request, response).getSubjectExams();
-        return "markssearch.jsp";
+        return "markssearch";
 	}
 
-	private String viewMarks() {
+	@PostMapping("/viewMarks")
+	public String viewMarks() {
 		if (new MarksDetailsService(request, response).viewMarks()) {
 			new MarksDetailsService(request, response).getSubjectExams();
-            return "markssearch.jsp";
+            return "markssearch";
         } else {
-            return "error.jsp";
+            return "error";
         }
 	}
 
-	private String addMarks() {
+	@PostMapping("/addMarks")
+	public String addMarks() {
 		String result = new MarksDetailsService(request, response).addMarks();
 		if (result=="true") {
-            return "markssaved.jsp";
+            return "markssaved";
         } else if(result=="Duplicate") {
-            return "erroraddingmarks.jsp";
+            return "erroraddingmarks";
         }else{
-        	return "error.jsp";
+        	return "error";
         }
         	
 	}
 	
-	private String search() {
+	@PostMapping("/search")
+	public String search() {
 		new MarksDetailsService(request, response).Search();
-        return "marksentry.jsp";
+        return "marksentry";
 	}
 	
+	@PostMapping("/searchForGraphicalReport")
 	private String searchForGraphicalReport() {
 		new MarksDetailsService(request, response).Search();
 		new MarksDetailsService(request, response).getStudentList();
-        return "graphicalreport.jsp";
+        return "graphicalreport";
 	}
 	
 }
