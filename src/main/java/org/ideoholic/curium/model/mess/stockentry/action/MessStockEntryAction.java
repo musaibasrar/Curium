29ae0e5c219516d95afe52491946e77d85f51858
Adeeba09 <fatemaadeeba@gmail.com>
2021-12-23 12:10:02 +0530

    Organized.

--- src/main/java/org/ideoholic/curium/model/mess/stockentry/action/MessStockEntryAction.java
+++ src/main/java/org/ideoholic/curium/model/mess/stockentry/action/MessStockEntryAction.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.ideoholic.curium.model.account.service.AccountService;
 import org.ideoholic.curium.model.mess.item.service.MessItemsService;
 import org.ideoholic.curium.model.mess.stockentry.service.MessStockEntryService;
 import org.ideoholic.curium.model.mess.supplier.service.MessSuppliersService;
@@ -34,61 +33,58 @@ public class MessStockEntryAction {
 	@Autowired
 	HttpServletResponse response;
 	@Autowired
-	HttpSession httpSession;        
+	HttpSession httpSession;
 
+	@GetMapping("/mrvDetails")
+	public void mrvDetails() {
 
-	    @GetMapping("/mrvDetails")
-        public void mrvDetails() {
-
-			try {
-				new MessStockEntryService(request, response).getMRVDetails();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		
+		try {
+			new MessStockEntryService(request, response).getMRVDetails();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
-        @PostMapping("/savePurchase")
-		public String savePurchase() {
-			
-        	return "purchase";
-		}
+	}
 
-        @PostMapping("/deleteItems")
-		public String deleteItems() {
-        	new MessItemsService(request, response).deleteMultipleItems();
-            return viewItems();
-		}
+	@PostMapping("/savePurchase")
+	public String savePurchase() {
 
-        @PostMapping("/updateItems")
-		public String updateItems() {
-			
-			new MessItemsService(request, response).updateItems();
-            return viewItems();
-		}
+		return "purchase";
+	}
 
-        @GetMapping("/viewItems")
-		public String viewItems() {
-        	return new MessItemsService(request, response).viewItemDetails();
-		}
+	@PostMapping("/deleteItems")
+	public String deleteItems() {
+		new MessItemsService(request, response).deleteMultipleItems();
+		return viewItems();
+	}
 
-        @PostMapping("/addItems")
-		public String addItems() {
-        		 new MessItemsService(request, response).addItemDetails();
-        		 return viewItems();
-        }
+	@PostMapping("/updateItems")
+	public String updateItems() {
+		new MessItemsService(request, response).updateItems();
+		return viewItems();
+	}
 
-        @GetMapping("/purchaseItems")
-        public String purchaseItems() {
-        	new MessSuppliersService(request, response).viewSuppliersDetails();
-        	new MessItemsService(request, response).viewItemDetails();
-                return "purchase";
-        }
-        
-        @RequestMapping(value = "/addSuppliers", method = { RequestMethod.GET, RequestMethod.POST })
-        public String addSuppliers() {
-            return "addsuppliers";
-    }
+	@GetMapping("/viewItems")
+	public String viewItems() {
+		return new MessItemsService(request, response).viewItemDetails();
+	}
 
+	@PostMapping("/addItems")
+	public String addItems() {
+		new MessItemsService(request, response).addItemDetails();
+		return viewItems();
+	}
+
+	@GetMapping("/purchaseItems")
+	public String purchaseItems() {
+		new MessSuppliersService(request, response).viewSuppliersDetails();
+		new MessItemsService(request, response).viewItemDetails();
+		return "purchase";
+	}
+
+	@RequestMapping(value = "/addSuppliers", method = { RequestMethod.GET, RequestMethod.POST })
+	public String addSuppliers() {
+		return "addsuppliers";
+	}
 
 }
