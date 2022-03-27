828ec5b1309fd0f0019170ece3654f70fac67489
Mushy <musaibasrar@gmail.com>
2022-03-27 22:17:09 +0530

    view all employee & print fees card

--- src/main/java/org/ideoholic/curium/model/employee/action/EmployeeAction.java
+++ src/main/java/org/ideoholic/curium/model/employee/action/EmployeeAction.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/EmployeeProcess")
@@ -59,7 +60,7 @@ public class EmployeeAction {
 		return "employee_details";
 	}
 
-	@PostMapping("/ViewAllEmployee")
+	@RequestMapping(value = "/ViewAllEmployee", method = { RequestMethod.GET, RequestMethod.POST })
 	public String viewEmployee() {
 		new EmployeeService(request, response).ViewAllEmployee();
 		return "viewAllEmployee";
