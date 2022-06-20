c24a7a6371da776d319b171f5c552c699a31b8e3
Mushy <musaibasrar@gmail.com>
2022-06-20 09:31:15 +0530

    added fees payment details to the receipt while printing from report

--- src/main/java/org/ideoholic/curium/model/feescollection/service/FeesCollectionService.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/service/FeesCollectionService.java
@@ -417,6 +417,8 @@ public class FeesCollectionService {
 			NumberToWord toWord = new NumberToWord();
 			String grandTotal = toWord.convert(rinfo.getTotalamount().intValue());
 			httpSession.setAttribute("grandTotal", grandTotal+" "+"Only");
+			
+			getFeesDetails(String.valueOf(rinfo.getSid()), rinfo.getAcademicyear());
 		}
 		
 	}
@@ -443,6 +445,8 @@ public class FeesCollectionService {
 			request.setAttribute("recieptinfo", rinfo);
 			request.setAttribute("feescatmap", feeCatMap);
 			request.setAttribute("duplicate", "duplicate");
+
+			getFeesDetails(String.valueOf(rinfo.getSid()), rinfo.getAcademicyear());
 			
 		}
 	}
