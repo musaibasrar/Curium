b53f081afa9bfafb22f7302779cea9a6b0959d45
Mushy <musaibasrar@gmail.com>
2022-07-31 22:56:25 +0530

    1. Academic Year selection during add student 2. Added academic year separately in receipt 3. Receipt Correction in total fees paid 4. Per page two receipt 5. added classsec in receipt info

--- src/main/java/org/ideoholic/curium/model/student/service/StudentService.java
+++ src/main/java/org/ideoholic/curium/model/student/service/StudentService.java
@@ -261,6 +261,9 @@ public class StudentService {
 		                if (fieldName.equalsIgnoreCase("rte")) {
 		                	student.setRte(DataUtil.parseInt(request.getParameter(fieldName)));
 		                }
+		                if (fieldName.equalsIgnoreCase("yearofadmission")) {
+		                	student.setYearofadmission(DataUtil.emptyString(request.getParameter(fieldName)));
+		                }
 		                // PU Details
 		                if (fieldName.equalsIgnoreCase("pep")) {
                                     puDetails.setExampassedappearance(DataUtil.parseInt(request.getParameter(fieldName)));
@@ -973,6 +976,9 @@ public class StudentService {
 	                if (fieldName.equalsIgnoreCase("droppedout")) {
 	                	student.setDroppedout(DataUtil.parseInt(request.getParameter(fieldName)));
 	                }
+	                if (fieldName.equalsIgnoreCase("yearofadmission")) {
+	                	student.setYearofadmission(DataUtil.emptyString(request.getParameter(fieldName)));
+	                }
 	                // Updating paretns information
 	                
 	                parents.setPid(parentsId);
@@ -1410,6 +1416,7 @@ public class StudentService {
 	public boolean promoteMultiple() {
 		String[] studentIds = request.getParameterValues("studentIDs");
 		String classStudying = request.getParameter("classstudying");
+		String promotedYear = httpSession.getAttribute("currentAcademicYear").toString();
 		List<Student> studentList = new ArrayList<Student>();
 		
 		boolean result = false;
@@ -1421,7 +1428,7 @@ public class StudentService {
 			studentList.add(student);
 		}
 		
-		if (new studentDetailsDAO().promoteMultiple(studentList, classStudying)) {
+		if (new studentDetailsDAO().promoteMultiple(studentList, classStudying, promotedYear)) {
 			result = true;
 		}
 		return result;
@@ -1475,7 +1482,7 @@ public class StudentService {
                     
                     long totalFeesAmount = 0l;
                     for (Studentfeesstructure studentfeesstructureSingle : feesstructure) {
-                            totalFeesAmount = totalFeesAmount+studentfeesstructureSingle.getFeesamount()-studentfeesstructureSingle.getWaiveoff();
+                            totalFeesAmount = totalFeesAmount+studentfeesstructureSingle.getFeesamount()-studentfeesstructureSingle.getWaiveoff()-studentfeesstructureSingle.getConcession();
                     }
                             httpSession.setAttribute("feesstructure", feesstructure);
                             httpSession.setAttribute("sumoffees", totalSum);
