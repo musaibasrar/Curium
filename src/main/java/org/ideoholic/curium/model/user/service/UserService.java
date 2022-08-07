c8f43cac0e9befaa4425bfe38171baa9ec200e17
Mushy <musaibasrar@gmail.com>
2022-08-07 16:11:58 +0530

    class studying check for dashboard

--- src/main/java/org/ideoholic/curium/model/user/service/UserService.java
+++ src/main/java/org/ideoholic/curium/model/user/service/UserService.java
@@ -108,10 +108,9 @@ public class UserService {
             for (Classsec classstudying : classsecList) {
         	
 		        	String classStudying = classstudying.getClassdetails();
+		    		if(!classStudying.equalsIgnoreCase("")) {
 		    		
-		    		if (!classStudying.equalsIgnoreCase("")) {
 		    			classStudying = classStudying+"--" +"%";
-		    		}
 		    		
                     List<Parents> student = new studentDetailsDAO().getStudentsList("FROM Parents as parents where parents.Student.classstudying like '"+classStudying+"'"
                     + " AND parents.Student.archive=0 AND parents.Student.passedout=0 AND parents.Student.droppedout=0 AND parents.Student.leftout=0 AND parents.Student.branchid='"+Integer.parseInt(httpSession.getAttribute(BRANCHID).toString())+"' ");
@@ -125,7 +124,7 @@ public class UserService {
                     }
                     
                 	}
-        
+        	}
         	// Total Teachers
         	List<Teacher> teacher = new EmployeeDAO().readCurrentTeachers(Integer.parseInt(httpSession.getAttribute(BRANCHID).toString()));
         	request.setAttribute("totalteachers", teacher.size());
