fb0f933e09aa44b17e0491ab25529ef32a2034f4
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2023-03-30 12:40:45 +0530

    Update test.java

--- src/main/java/org/ideoholic/curium/controller/test.java
+++ src/main/java/org/ideoholic/curium/controller/test.java
@@ -114,7 +114,7 @@ public class test {
 			byte[] bytes = new byte[1024];
 
 			while ((read = inputStream.read(bytes)) != -1) {
-				outputStream.write(bytes, 0, read);
+				outputStream.write(bytes, 0, read);;
 			}
 			
 			
