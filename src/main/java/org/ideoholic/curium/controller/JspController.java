f5fad32f4b6094037c9dbfe513bb46bea4b1f804
Mushy <musaibasrar@gmail.com>
2023-08-21 08:12:22 +0530

    added feature for Concession and waive off of other fees

--- src/main/java/org/ideoholic/curium/controller/JspController.java
+++ src/main/java/org/ideoholic/curium/controller/JspController.java
@@ -420,4 +420,9 @@ public class JspController {
 		return "printpreviewemployee";
 	}
 	
+	@GetMapping("/student_details_other_feesstructure")
+	public String getStudentDetailsOtherFeesstructure() {
+		return "student_details_other_feesstructure";
+	}
+	
 }
