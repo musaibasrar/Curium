b0ada07822309bdb68573e856b1149ffa5ea44a2
tauqueer031 <mdtauqueeralam031@gmail.com>
2024-01-25 22:46:08 +0530

    Enquiry Module Added

---
+++ src/main/java/org/ideoholic/curium/model/enquiry/action/EnquiryAction.java
@@ -0,0 +1,37 @@
+package org.ideoholic.curium.model.enquiry.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.ideoholic.curium.model.documents.service.DocumentService;
+import org.ideoholic.curium.model.enquiry.service.EnquiryService;
+import org.ideoholic.curium.model.std.service.StandardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/EnquiryProcess")
+public class EnquiryAction {
+	@Autowired
+	HttpServletRequest request;
+	@Autowired
+	HttpServletResponse response;
+	@Autowired
+	HttpSession httpSession;
+	
+	@GetMapping("/newCertificate")
+	public String NewCertificateDetail() {
+		new StandardService(request, response).viewClasses(); 
+		return "newcertificate";
+	}
+
+	@PostMapping("/genarateNewCertificate")
+	public String genarateNewCertificate() {
+		new EnquiryService(request, response).getCertificate(); 
+		return "newcertificatepreview";
+	}
+	}
