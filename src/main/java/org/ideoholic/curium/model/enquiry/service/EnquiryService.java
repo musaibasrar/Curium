00af09847ac1835188bea5966233441ab9fd721f
Mushy <musaibasrar@gmail.com>
2024-03-06 09:06:07 +0530

    Enquiry Module Changes

--- src/main/java/org/ideoholic/curium/model/enquiry/service/EnquiryService.java
+++ src/main/java/org/ideoholic/curium/model/enquiry/service/EnquiryService.java
@@ -1,13 +1,16 @@
 package org.ideoholic.curium.model.enquiry.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.ideoholic.curium.model.diary.dao.diaryDAO;
-import org.ideoholic.curium.model.diary.dto.Diary;
-import org.ideoholic.curium.model.enquiry.dao.enquiryDAO;
+import org.ideoholic.curium.model.enquiry.dao.EnquiryDAO;
 import org.ideoholic.curium.model.enquiry.dto.Enquiry;
+import org.ideoholic.curium.model.job.dao.JobDAO;
+import org.ideoholic.curium.model.task.dto.Task;
 import org.ideoholic.curium.util.DataUtil;
 import org.ideoholic.curium.util.DateUtil;
 
@@ -46,8 +49,170 @@ public class EnquiryService {
 	    enquiry.setAddress(place);
 	    enquiry.setMobileno(mobile);
 	          
-                enquiry =  new enquiryDAO().create(enquiry);
+                enquiry =  new EnquiryDAO().create(enquiry);
                 }
+
+
+
+	public void saveEnquiry() {
+		
+		
+	    String name= request.getParameter("name");		
+	    String place= request.getParameter("place");	
+	    String mobile= request.getParameter("mobile");
+	    String fatherName= request.getParameter("fathername");
+	    String motherName= request.getParameter("mothername");
+	    String admissionClass= request.getParameter("admissionclass");
+	    String createdDate= request.getParameter("createddate");
+	    String siblings= request.getParameter("siblings");
+	    String academicYear = request.getParameter("yearofadmission");
+	    Enquiry enquiry = new Enquiry();
+	    enquiry.setName(name);
+	    enquiry.setAddress(place);
+	    enquiry.setMobileno(mobile);
+	    enquiry.setFathername(fatherName);
+	    enquiry.setMothername(motherName);
+	    enquiry.setAdmissionclass(admissionClass);
+	    enquiry.setCreateddate(DateUtil.indiandateParser(createdDate));
+	    enquiry.setSiblings(siblings);
+	    enquiry.setAcademicyear(academicYear);
+	    enquiry.setStatus("Processing");
+	    enquiry.setBranchid(Integer.parseInt(httpSession.getAttribute("branchid").toString()));
+	    enquiry.setUserid(Integer.parseInt(httpSession.getAttribute("userloginid").toString()));
+        enquiry =  new EnquiryDAO().create(enquiry);
+       
+	}
+
+
+
+	public boolean getEnquiries() {
+
+
+		boolean result = false;
+		//String pages = "1";
+		if(httpSession.getAttribute("branchid")!=null){
+			try {
+				int page = 1;
+				int recordsPerPage = 500;
+					if (!"".equalsIgnoreCase(DataUtil.emptyString(request.getParameter("page")))) {
+						page = Integer.parseInt(request.getParameter("page"));
+					}
+
+				List<Enquiry> list = new EnquiryDAO().readListOfObjectsPaginationEnquiry((page - 1) * recordsPerPage,
+						recordsPerPage, Integer.parseInt(httpSession.getAttribute("branchid").toString()));
+				int noOfRecords = new EnquiryDAO().getNoOfRecordsEnquiry(Integer.parseInt(httpSession.getAttribute("branchid").toString()));
+				int noOfPages = (int) Math.ceil(noOfRecords * 1.0 / recordsPerPage);
+				request.setAttribute("enquirylist", list);
+				request.setAttribute("noOfPages", noOfPages);
+				request.setAttribute("currentPage", page);
+				result = true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	
+		
+	}
+
+
+
+	public void approved() {
+		
+		String[] enquiryIds = request.getParameterValues("enquiryids");
+		int userId = Integer.parseInt(httpSession.getAttribute("userloginid").toString());
+		List<Integer> enquiryIdsList = new ArrayList<Integer>();
+		boolean result = false;
+		
+		if(enquiryIds!=null) {
+			for (String ids : enquiryIds) {
+				enquiryIdsList.add(Integer.parseInt(ids));
+			}
+			
+			result = new EnquiryDAO().updateStatus(enquiryIdsList, userId, "Approved");
+			
+			
+			if(result){
+				request.setAttribute("querycompleted","success");
+				request.setAttribute("querystatus",true);
+			}else {
+				request.setAttribute("querystatus",false);
+			}
+		}
+	}
+	
+	public void rejected() {
+			
+			String[] enquiryIds = request.getParameterValues("enquiryids");
+			int userId = Integer.parseInt(httpSession.getAttribute("userloginid").toString());
+			List<Integer> enquiryIdsList = new ArrayList<Integer>();
+			boolean result = false;
+			
+			if(enquiryIds!=null) {
+				for (String ids : enquiryIds) {
+					enquiryIdsList.add(Integer.parseInt(ids));
+				}
+				
+				result = new EnquiryDAO().updateStatus(enquiryIdsList, userId, "Rejected");
+				
+				
+				if(result){
+					request.setAttribute("querycompleted","success");
+					request.setAttribute("querystatus",true);
+				}else {
+					request.setAttribute("querystatus",false);
+				}
+			}
+		}
+	
+	public void processing() {
+			
+			String[] enquiryIds = request.getParameterValues("enquiryids");
+			int userId = Integer.parseInt(httpSession.getAttribute("userloginid").toString());
+			List<Integer> enquiryIdsList = new ArrayList<Integer>();
+			boolean result = false;
+			
+			if(enquiryIds!=null) {
+				for (String ids : enquiryIds) {
+					enquiryIdsList.add(Integer.parseInt(ids));
+				}
+				
+				result = new EnquiryDAO().updateStatus(enquiryIdsList, userId, "Processing");
+				
+				
+				if(result){
+					request.setAttribute("querycompleted","success");
+					request.setAttribute("querystatus",true);
+				}else {
+					request.setAttribute("querystatus",false);
+				}
+			}
+		}
+	
+		public void onHold() {
+				
+				String[] enquiryIds = request.getParameterValues("enquiryids");
+				int userId = Integer.parseInt(httpSession.getAttribute("userloginid").toString());
+				List<Integer> enquiryIdsList = new ArrayList<Integer>();
+				boolean result = false;
+				
+				if(enquiryIds!=null) {
+					for (String ids : enquiryIds) {
+						enquiryIdsList.add(Integer.parseInt(ids));
+					}
+					
+					result = new EnquiryDAO().updateStatus(enquiryIdsList, userId, "On Hold");
+					
+					
+					if(result){
+						request.setAttribute("querycompleted","success");
+						request.setAttribute("querystatus",true);
+					}else {
+						request.setAttribute("querystatus",false);
+					}
+				}
+			}
 	   
 		}
 
