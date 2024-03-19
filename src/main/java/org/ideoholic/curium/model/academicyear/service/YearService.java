92dbfceb4491a4f1d52ac0f028c6576eda58b4e2
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2024-03-19 11:14:29 +0530

    Academic year Mapper

--- src/main/java/org/ideoholic/curium/model/academicyear/service/YearService.java
+++ src/main/java/org/ideoholic/curium/model/academicyear/service/YearService.java
@@ -5,7 +5,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.ideoholic.curium.model.academicyear.dao.YearDAO;
+import org.ideoholic.curium.model.academicyear.dto.CurrentAcademicYearDto;
 import org.ideoholic.curium.model.academicyear.dto.Currentacademicyear;
+import org.ideoholic.curium.model.academicyear.dto.Yearmapper;
+import org.ideoholic.curium.model.student.dto.Student;
+import org.ideoholic.curium.model.student.dto.StudentMapper;
 import org.ideoholic.curium.util.DataUtil;
 
 public class YearService {
@@ -39,7 +43,18 @@ public class YearService {
 	}
 
 
-	
+	public CurrentAcademicYearDto saveYear(CurrentAcademicYearDto current) {
+		boolean result=false;
+		String errorService=null;
+		Currentacademicyear currentacademicyear = Yearmapper.INSTANCE.mapCurrentacademicyear(current);
+		//Currentacademicyear currentacademicyear = new Currentacademicyear();
+		//currentacademicyear.setCurrentacademicyear(DataUtil.emptyString(request.getParameter("academicyear")));
+		errorService=new YearDAO().create(currentacademicyear);
+		
+		
+            return Yearmapper.INSTANCE.mapCurrentAcademicYearDto(currentacademicyear);
+		
+	}	
 
 	public boolean updateYear() {
 		Currentacademicyear currentacademicyear = new Currentacademicyear();
