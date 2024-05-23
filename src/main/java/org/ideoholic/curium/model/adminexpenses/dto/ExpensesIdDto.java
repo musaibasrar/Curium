7ecb3df22f99f3a55434ca38e8a7af69728cd256
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2024-05-23 15:41:54 +0530

    Migrating AdminExpenses rejectVoucher using adaptor (#195)
    * Migrating AdminExpenses rejectVoucher using adaptor
    
    * Migrating AdminExpenses rejectVoucher using adaptor updated

---
+++ src/main/java/org/ideoholic/curium/model/adminexpenses/dto/ExpensesIdDto.java
@@ -0,0 +1,15 @@
+package org.ideoholic.curium.model.adminexpenses.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExpensesIdDto {
+
+	private String[] expensesIds;
+}
