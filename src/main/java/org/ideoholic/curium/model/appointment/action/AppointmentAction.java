84d4e957b265616e2adc9980f53767a2d87e9952
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-05-28 19:05:32 +0530

    Migrating Appointment addAppointmet using Adapter (#205)

--- src/main/java/org/ideoholic/curium/model/appointment/action/AppointmentAction.java
+++ src/main/java/org/ideoholic/curium/model/appointment/action/AppointmentAction.java
@@ -93,8 +93,7 @@ public class AppointmentAction {
 
 	@PostMapping("/addAppointment")
 	private String addAppointment() {
-		
-		if(new AppointmentService(request, response).addAppointment()){
+		if(appointmentActionAdapter.addAppointment()){
 			new StudentService(request, response).viewAllStudentsParents();
 			new EmployeeService(request, response).ViewAllEmployee();
 			return "viewAllWithParents";
