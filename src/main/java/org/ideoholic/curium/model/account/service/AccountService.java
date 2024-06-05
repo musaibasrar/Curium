26bb209440b8de336b440da46e77625903b9f76a
Shaik Shafeeq Ahmed <shafeeq7.cse@gmail.com>
2024-06-05 11:02:17 +0530

    Migrating AccountAction getSSGroupNames using adapter.

--- src/main/java/org/ideoholic/curium/model/account/service/AccountService.java
+++ src/main/java/org/ideoholic/curium/model/account/service/AccountService.java
@@ -1032,61 +1032,67 @@ public class AccountService {
 	}
 
 
-	public void getSSGroupNames() throws IOException {
+	public ResultResponse getSSGroupNames(String branchId, String strAccountSubGroupMasterId) throws IOException {
 
 		List<Accountssgroupmaster> accountSSGroupMaster = new ArrayList<Accountssgroupmaster>();
 
-		if(httpSession.getAttribute(BRANCHID)!=null){
+		if(branchId!=null){
 			PrintWriter out = response.getWriter();
 			response.setContentType("text/xml");
-		    response.setHeader("Cache-Control", "no-cache");
+			response.setHeader("Cache-Control", "no-cache");
 
-			if(!"New Group".equalsIgnoreCase(request.getParameter("subgroupname").toString())) {
-			int accountSubGroupMasterId = DataUtil.parseInt(request.getParameter("subgroupname"));
-			accountSSGroupMaster = new AccountDAO().getListAccountSSGroupMaster(accountSubGroupMasterId,Integer.parseInt(httpSession.getAttribute(BRANCHID).toString()));
-			request.setAttribute("accountssgroupmaster", accountSSGroupMaster);
-		        try {
+			if(!"New Group".equalsIgnoreCase(strAccountSubGroupMasterId)) {
+				int accountSubGroupMasterId = DataUtil.parseInt(strAccountSubGroupMasterId);
+				accountSSGroupMaster = new AccountDAO().getListAccountSSGroupMaster(accountSubGroupMasterId,Integer.parseInt(branchId));
+				ResultResponse
+						.builder()
+						.resultList(accountSSGroupMaster)
+						.build();
+				try {
 
-		        	if(!accountSSGroupMaster.isEmpty()){
-		        		String buffer = "<select name='ssgroupname' style='width: 240px' id='ssgname' onchange='ssGroupSelect()'>";
-		        		buffer = buffer+"<option></option>";
-			        	for(int i =0; i<accountSSGroupMaster.size();i++){
-			        		buffer = buffer +  "<option value="+accountSSGroupMaster.get(i).getSsgroupmasterid()+">"+accountSSGroupMaster.get(i).getSsgroupname()+"</option>";
-			        	}
-			        	buffer = buffer+"<option value='New Sub-Group'>New Sub-Group</option></select>";
-			        	response.getWriter().println(buffer);
-		        	}else{
-		        		String buffer = "<select name='ssgroupname' style='width: 240px' id='ssgname' onchange='ssGroupSelect()'>";
-		        		buffer = buffer+"<option></option>";
-			        	buffer = buffer+"<option value='New Sub-Group'>New Sub-Group</option>";
-			        	buffer = buffer+"</select>";
-			        	response.getWriter().println(buffer);
-		        	}
+					if(!accountSSGroupMaster.isEmpty()){
+						String buffer = "<select name='ssgroupname' style='width: 240px' id='ssgname' onchange='ssGroupSelect()'>";
+						buffer = buffer+"<option></option>";
+						for(int i =0; i<accountSSGroupMaster.size();i++){
+							buffer = buffer +  "<option value="+accountSSGroupMaster.get(i).getSsgroupmasterid()+">"+accountSSGroupMaster.get(i).getSsgroupname()+"</option>";
+						}
+						buffer = buffer+"<option value='New Sub-Group'>New Sub-Group</option></select>";
+						response.getWriter().println(buffer);
+					}else{
+						String buffer = "<select name='ssgroupname' style='width: 240px' id='ssgname' onchange='ssGroupSelect()'>";
+						buffer = buffer+"<option></option>";
+						buffer = buffer+"<option value='New Sub-Group'>New Sub-Group</option>";
+						buffer = buffer+"</select>";
+						response.getWriter().println(buffer);
+					}
 
-		        } catch (Exception e) {
-		            out.write("<subgroup>0</subgroup>");
-		        } finally {
-		            out.flush();
-		            out.close();
-		        }
-		}else {
-			try {
-	        		String buffer = "<select name='ssgroupname' style='width: 240px' id='ssgname' onchange='ssGroupSelect()'>";
-	        		buffer = buffer+"<option></option>";
-		        	buffer = buffer+"<option value='New Sub-Group'>New Sub-Group</option>";
-		        	buffer = buffer+"</select>";
-		        	response.getWriter().println(buffer);
+				} catch (Exception e) {
+					out.write("<subgroup>0</subgroup>");
+				} finally {
+					out.flush();
+					out.close();
+				}
+			}else {
+				try {
+					String buffer = "<select name='ssgroupname' style='width: 240px' id='ssgname' onchange='ssGroupSelect()'>";
+					buffer = buffer+"<option></option>";
+					buffer = buffer+"<option value='New Sub-Group'>New Sub-Group</option>";
+					buffer = buffer+"</select>";
+					response.getWriter().println(buffer);
 
-	        } catch (Exception e) {
-	            out.write("<subgroup>0</subgroup>");
-	        } finally {
-	            out.flush();
-	            out.close();
-	        }
+				} catch (Exception e) {
+					out.write("<subgroup>0</subgroup>");
+				} finally {
+					out.flush();
+					out.close();
+				}
+			}
 		}
-		}
-		
-		
+
+
+		return ResultResponse
+				.builder()
+				.build();
 	}
 
 
