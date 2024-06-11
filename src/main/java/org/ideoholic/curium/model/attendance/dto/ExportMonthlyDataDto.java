e76e0ba794686937f2dc091c890223bab0641d01
Shaik Shafeeq Ahmed <144222899+shafeeq-cse@users.noreply.github.com>
2024-06-11 15:02:47 +0530

    Migrating AttendanceAction exportMonthlyData using adapter.

---
+++ src/main/java/org/ideoholic/curium/model/attendance/dto/ExportMonthlyDataDto.java
@@ -0,0 +1,10 @@
+package org.ideoholic.curium.model.attendance.dto;
+
+import lombok.Data;
+
+@Data
+public class ExportMonthlyDataDto {
+    private String addClass;
+    private String addSec;
+    private String monthOf;
+}
