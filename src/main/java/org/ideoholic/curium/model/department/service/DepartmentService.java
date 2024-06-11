b255e854264c2f77ccb06d42d856bf944eb16165
Md Shahnawaz Ahmad <shahnawazahmad1050@gmail.com>
2024-06-11 13:12:48 +0530

    Migrated Department addDepartment using Adapter

--- src/main/java/org/ideoholic/curium/model/department/service/DepartmentService.java
+++ src/main/java/org/ideoholic/curium/model/department/service/DepartmentService.java
@@ -8,46 +8,46 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.ideoholic.curium.model.department.dao.departmentDAO;
+import org.ideoholic.curium.model.department.dto.AddDepartmentDto;
 import org.ideoholic.curium.model.department.dto.Department;
 import org.ideoholic.curium.model.employee.dao.EmployeeDAO;
 import org.ideoholic.curium.util.DataUtil;
 
 public class DepartmentService {
 
-	private HttpServletRequest request;
-	private HttpServletResponse response;
-	private HttpSession httpSession;
-	private String BRANCHID = "branchid";
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private HttpSession httpSession;
+    private String BRANCHID = "branchid";
 
-	public DepartmentService(HttpServletRequest request,
-			HttpServletResponse response) {
+    public DepartmentService(HttpServletRequest request,
+                             HttpServletResponse response) {
 
-		this.request = request;
-		this.response = response;
-		this.httpSession = request.getSession();
+        this.request = request;
+        this.response = response;
+        this.httpSession = request.getSession();
 
-	}
+    }
 
-	public void addDepartment() {
-		
-		
-		Department department = new Department();
-		if(httpSession.getAttribute(BRANCHID)!=null){
-			
-			department.setDepartmentname(DataUtil.emptyString(request.getParameter("department")));
-			department.setBranchid(Integer.parseInt(httpSession.getAttribute(BRANCHID).toString()));
-			
-			if(!department.getDepartmentname().equalsIgnoreCase("")){
-				department =  new departmentDAO().create(department);
-			}
-			
-		}
-	}
+    public void addDepartment(AddDepartmentDto addDepartmentDto) {
 
-	public boolean viewDepartment() {
-		boolean result = false;
+        Department department = new Department();
+        if (addDepartmentDto.getBranchId() != null) {
+
+            department.setDepartmentname(DataUtil.emptyString(addDepartmentDto.getDepartment()));
+            department.setBranchid(Integer.parseInt(addDepartmentDto.getBranchId()));
+
+            if (!department.getDepartmentname().equalsIgnoreCase("")) {
+                department = new departmentDAO().create(department);
+            }
+
+        }
+    }
+
+    public boolean viewDepartment() {
+        boolean result = false;
         try {
-        	List<Department> list = new departmentDAO().readListOfObjects(Integer.parseInt(httpSession.getAttribute(BRANCHID).toString()));
+            List<Department> list = new departmentDAO().readListOfObjects(Integer.parseInt(httpSession.getAttribute(BRANCHID).toString()));
             httpSession.setAttribute("departmentList", list);
 
             result = true;
@@ -56,22 +56,22 @@ public class DepartmentService {
             result = false;
         }
         return result;
-	}
+    }
 
-	public void deleteMultiple() {
-		 String[] departmentIds = request.getParameterValues("departmentIDs");
-		 if(departmentIds!=null){
-			 
-		 
-	        List<Integer> ids = new ArrayList();
-	        for (String id : departmentIds) {
-	            System.out.println("id" + id);
-	            ids.add(Integer.valueOf(id));
+    public void deleteMultiple() {
+        String[] departmentIds = request.getParameterValues("departmentIDs");
+        if (departmentIds != null) {
 
-	        }
-	        System.out.println("id length" + departmentIds.length);
-	        new departmentDAO().deleteMultiple(ids);
-		 }
-	}
+
+            List<Integer> ids = new ArrayList();
+            for (String id : departmentIds) {
+                System.out.println("id" + id);
+                ids.add(Integer.valueOf(id));
+
+            }
+            System.out.println("id length" + departmentIds.length);
+            new departmentDAO().deleteMultiple(ids);
+        }
+    }
 
 }
