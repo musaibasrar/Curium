eb887f7bd64c48cd8a6cd69c76618a23f7041d2a
Shaik Shafeeq Ahmed <shafeeq7.cse@gmail.com>
2024-06-12 13:01:37 +0530

    Migrating AttendanceAction viewStudentAttendanceDetailsMark using adapter.

---
+++ src/main/java/org/ideoholic/curium/model/attendance/dto/ViewStudentAttendanceDetailsMarkDto.java
@@ -0,0 +1,10 @@
+package org.ideoholic.curium.model.attendance.dto;
+
+import lombok.Data;
+
+@Data
+public class ViewStudentAttendanceDetailsMarkDto {
+    private String studentName;
+    private String addClass;
+    private String addSec;
+}
