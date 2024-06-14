9098e77a35a9089c301562bdc2148a38bc820a13
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-06-14 09:16:47 +0530

    Migrated Diary addDiary using Adapter (#297)

---
+++ src/main/java/org/ideoholic/curium/model/diary/action/DiaryActionAdapter.java
@@ -0,0 +1,39 @@
+package org.ideoholic.curium.model.diary.action;
+
+import org.ideoholic.curium.model.diary.dto.AddDiaryDto;
+import org.ideoholic.curium.model.diary.service.Diaryservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+@Service
+public class DiaryActionAdapter {
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private HttpServletResponse response;
+    @Autowired
+    private HttpSession httpSession;
+
+    private String BRANCHID = "branchid";
+
+    public void addDiary() {
+        Diaryservice diaryservice = new Diaryservice(request, response);
+        AddDiaryDto addDiaryDto = new AddDiaryDto();
+        addDiaryDto.setAddSec(request.getParameter("addsec"));
+        addDiaryDto.setAddClass(request.getParameter("addclass"));
+        addDiaryDto.setMessageBody(request.getParameter("messagebody"));
+        addDiaryDto.setSubject(request.getParameter("subject"));
+        addDiaryDto.setCreatedDate(request.getParameter("createddate"));
+        addDiaryDto.setEndDate(request.getParameter("enddate"));
+        addDiaryDto.setStartDate(request.getParameter("startdate"));
+
+
+        diaryservice.addDiary(addDiaryDto, httpSession.getAttribute(BRANCHID).toString(),
+                                           httpSession.getAttribute("userloginid").toString(),
+                                           httpSession.getAttribute("currentAcademicYear").toString());
+
+    }
+}
