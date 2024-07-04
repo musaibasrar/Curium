56184a6a92b62fbefd0c586ad49467559d302429
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2024-07-04 15:05:46 +0530

    Creating YearApiAction based on YearAction
    * Creating YearApiAction based on YearAction
    
    * Creating YearApiAction based on YearAction updated

--- src/main/java/org/ideoholic/curium/model/academicyear/action/YearActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/academicyear/action/YearActionAdapter.java
@@ -23,9 +23,11 @@ public class YearActionAdapter {
 
 	@Autowired
 	private HttpSession httpSession;
+	
+	@Autowired
+	private YearService yearService;
 
 	public boolean saveYear() {
-		YearService yearService = new YearService(request, response);
 
 		CurrentAcademicYearDto currentacademicyeardto = new CurrentAcademicYearDto();
 		currentacademicyeardto.setCurrentacademicyear(request.getParameter("academicyear"));
@@ -40,13 +42,11 @@ public class YearActionAdapter {
 	}
 
 	public void updateYear() {
-		YearService yearService = new YearService(request, response);
 		CurrentAcademicYearResponseDto currentacademicyear = yearService.updateYear();
 		request.setAttribute("currentyear", currentacademicyear.getCurrentacademicyear());
 	}
 	
 	public boolean getYear() {
-		YearService yearService = new YearService(request, response);
 		Currentacademicyear currentYear = yearService.getYear();
 		if(currentYear == null) {
 			return false;
