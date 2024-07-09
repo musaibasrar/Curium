fb6625443305e68e2ff42d2387851ddeedf7cde0
Tauqueer <mdtauqueeralam031@gmail.com>
2024-07-09 09:27:32 +0530

    Migrating FeesAction searchFeesWaiveofforConcessionReport using Adapter

--- src/main/java/org/ideoholic/curium/model/feescategory/action/FeesActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/feescategory/action/FeesActionAdapter.java
@@ -9,7 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.ideoholic.curium.model.documents.dto.SearchStudentDto;
 import org.ideoholic.curium.model.feescategory.dto.ConcessionDto;
+import org.ideoholic.curium.model.feescategory.dto.SearchFeesResponseDto;
 import org.ideoholic.curium.model.student.dto.StudentIdDto;
 import org.ideoholic.curium.model.feescategory.service.FeesService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +48,18 @@ public class FeesActionAdapter {
 			 String studentId = studentIdDto.getStudentId();
 			 return studentId;
 			 
+	}
+
+	public void searchFeesWaiveofforConcessionReport(String waiveoff) {
+		FeesService feesService = new FeesService(request, response);
+		SearchStudentDto searchStudentDto = new SearchStudentDto();
+		searchStudentDto.setNameSearch(request.getParameter("namesearch"));
+		searchStudentDto.setClassSearch(request.getParameter("classsearch"));
+		searchStudentDto.setSecSearch(request.getParameter("secsearch"));
+		SearchFeesResponseDto searchFeesResponseDto = feesService.searchFeesWaiveofforConcessionReport(searchStudentDto,waiveoff,httpSession.getAttribute("branchid").toString());
+		httpSession.setAttribute("currentyearfromservice",searchFeesResponseDto.getCurrentYearFromService());
+		httpSession.setAttribute("studentsfeesstructuredetailswaiveoff", searchFeesResponseDto.getStudentsFeesStructureDetailsWaiveoff());
+		httpSession.setAttribute("studentsfeesstructuredetailsconcession", searchFeesResponseDto.getStudentsFeesStructureDetailsConcession());
 	}
 	
 }
