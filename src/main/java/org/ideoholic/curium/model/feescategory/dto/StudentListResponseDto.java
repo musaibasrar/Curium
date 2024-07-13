a08a9c406f5c46ef2bfec33e868e01bfa7793d8c
Tauqueer <mdtauqueeralam031@gmail.com>
2024-07-13 15:45:57 +0530

    Migrating FeesAction viewAllBranchStudents using Adapter

---
+++ src/main/java/org/ideoholic/curium/model/feescategory/dto/StudentListResponseDto.java
@@ -0,0 +1,18 @@
+package org.ideoholic.curium.model.feescategory.dto;
+
+import java.util.List;
+
+import org.ideoholic.curium.model.student.dto.Student;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentListResponseDto {
+		private List<Student> studentListFeesCollection;
+}
