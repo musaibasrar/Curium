89245b5622ace95d839db5d2b73a08696a84a845
Tauqueer <mdtauqueeralam031@gmail.com>
2024-07-23 09:56:10 +0530

    Migrating FeesAction viewOtherFees, getFeeCategory and applyotherConcession using Adapter changed

--- src/main/java/org/ideoholic/curium/model/feescategory/dto/OtherFeesCategoryResponseDto.java
+++ src/main/java/org/ideoholic/curium/model/feescategory/dto/OtherFeesCategoryResponseDto.java
@@ -2,6 +2,8 @@ package org.ideoholic.curium.model.feescategory.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
 public class OtherFeesCategoryResponseDto {
          
 	private List<OtherFeecategory> otherFeesCategory;
