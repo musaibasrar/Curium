fc8e4349c55e45cfba7116ecaacf5cdf543c97dc
Shaik Shafeeq Ahmed <144222899+shafeeq-cse@users.noreply.github.com>
2024-07-26 11:50:24 +0530

    Migrating MessSuppliersAction viewSuppliersPaymentDetails and updateSuppliers using adapter.

--- src/main/java/org/ideoholic/curium/model/mess/item/dto/MessIdsDto.java
+++ src/main/java/org/ideoholic/curium/model/mess/item/dto/MessIdsDto.java
@@ -2,7 +2,10 @@ package org.ideoholic.curium.model.mess.item.dto;
 
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class MessIdsDto {
     private String[] messIds;
+    private Map<String, String> requestParams;
 }
