bffd6e43df99422a7b2ceebc61c9fd526a7a1081
Md Shahnawaz Ahmad <shahnawazahmad1050@gmail.com>
2024-07-29 12:43:34 +0530

    Updated ExamDetails printPreviewHallTicket using Adapter

--- src/main/java/org/ideoholic/curium/model/examdetails/dto/HallTicketResponseDto.java
+++ src/main/java/org/ideoholic/curium/model/examdetails/dto/HallTicketResponseDto.java
@@ -1,11 +1,13 @@
 package org.ideoholic.curium.model.examdetails.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import org.ideoholic.curium.model.parents.dto.Parents;
 
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
 public class HallTicketResponseDto {
     private List<Parents> studentList ;
     private String examname;
