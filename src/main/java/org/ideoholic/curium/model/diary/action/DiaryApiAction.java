9862f4298c8d6331717b0a15476ab354e802dd39
Tauqueer <mdtauqueeralam031@gmail.com>
2024-07-30 15:51:44 +0530

    Creating DiaryApiAction based on DiaryAction

---
+++ src/main/java/org/ideoholic/curium/model/diary/action/DiaryApiAction.java
@@ -0,0 +1,94 @@
+package org.ideoholic.curium.model.diary.action;
+
+import org.ideoholic.curium.dto.ResultResponse;
+import org.ideoholic.curium.model.diary.dto.AddDiaryDto;
+import org.ideoholic.curium.model.diary.dto.DairyIdsDto;
+import org.ideoholic.curium.model.diary.dto.DiaryResponseDto;
+import org.ideoholic.curium.model.diary.dto.ViewDetailsOfDiaryMessageResponseDto;
+import org.ideoholic.curium.model.diary.service.DiaryService;
+import org.ideoholic.curium.model.std.service.StandardService;
+import org.ideoholic.curium.model.student.dto.StudentIdDto;
+import org.ideoholic.curium.model.student.dto.StudentIdPageDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/api/v1/DiaryProcess")
+public class DiaryApiAction {
+	@Autowired
+	private DiaryService diaryService;
+	@Autowired
+	private StandardService standardService;
+
+	@GetMapping("/getdiarystudent")
+	public ResponseEntity<ResultResponse> getdiarystudent(@RequestHeader(value = "branchid") String branchId) {
+		ResultResponse result = standardService.viewClasses(branchId);
+		return ResponseEntity.ok(result);
+
+	}
+
+	@PostMapping("/addDiary")
+	public ResponseEntity addDiary(@RequestBody AddDiaryDto addDiaryDto,
+			@RequestHeader(value = "branchid") String branchId,
+			@RequestHeader(value = "currentAcademicYear") String currentAcademicYear,
+			@RequestHeader(value = "userloginid") String userLoginId) {
+		diaryService.addDiary(addDiaryDto, branchId, currentAcademicYear, userLoginId);
+		return ResponseEntity.ok().build();
+
+	}
+
+	@RequestMapping(value = "/viewdiarystudent", method = { RequestMethod.GET, RequestMethod.POST })
+	public ResponseEntity<DiaryResponseDto> viewdiarystudent(@RequestHeader(value = "branchid") String branchId,
+			String strPage) {
+		DiaryResponseDto result = diaryService.viewDiary(branchId, strPage);
+		return ResponseEntity.ok(result);
+	}
+
+	@RequestMapping(value = "/viewDiaryStudentParent", method = { RequestMethod.GET, RequestMethod.POST })
+	public ResponseEntity<DiaryResponseDto> viewDiaryStudentParent(@RequestBody StudentIdPageDto studentIdPageDto,
+			@RequestHeader(value = "branchid") String branchId) {
+		DiaryResponseDto result = diaryService.viewDiaryParent(studentIdPageDto, branchId);
+		return ResponseEntity.ok(result);
+	}
+
+	@PostMapping("/deleteRecord")
+	public ResponseEntity<DiaryResponseDto> deleteRecord(@RequestBody DairyIdsDto dairyIdsDto,
+			@RequestHeader(value = "branchid") String branchId) {
+		diaryService.deleteRecord(dairyIdsDto);
+		DiaryResponseDto result = diaryService.viewDiary(branchId, dairyIdsDto.getPage());
+		return ResponseEntity.ok(result);
+	}
+
+	@PostMapping("/diarySaved")
+	public ResponseEntity<String> diarySaved() {
+		
+		return ResponseEntity.ok("viewDiary");
+	}
+
+	@PostMapping("/ViewDiaryDetails")
+	public ResponseEntity<ViewDetailsOfDiaryMessageResponseDto> ViewDiaryDetails(
+			@RequestBody StudentIdDto studentIdDto) {
+		ViewDetailsOfDiaryMessageResponseDto result = diaryService.viewDetailsOfDiaryMessage(studentIdDto);
+		if (result.isSuccess()) {
+
+		}
+		return ResponseEntity.ok(result);
+	}
+
+	@PostMapping("/ViewDiaryDetailsParent")
+	public ResponseEntity<ViewDetailsOfDiaryMessageResponseDto> ViewDiaryDetailsParent(
+			@RequestBody StudentIdDto studentIdDto) {
+		ViewDetailsOfDiaryMessageResponseDto result = diaryService.viewDetailsOfDiaryMessage(studentIdDto);
+		if (result.isSuccess()) {
+
+		}
+		return ResponseEntity.ok(result);
+	}
+}
--- src/main/java/org/ideoholic/curium/model/diary/dto/DairyIdsDto.java
+++ src/main/java/org/ideoholic/curium/model/diary/dto/DairyIdsDto.java
@@ -5,4 +5,5 @@ import lombok.Data;
 @Data
 public class DairyIdsDto {
     private String[] idDiary;
+    private String page;
 }
