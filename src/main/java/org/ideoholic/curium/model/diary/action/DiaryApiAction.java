0cb41fb898549695e1771fd824ace4609e95bbab
Tauqueer <mdtauqueeralam031@gmail.com>
2024-08-02 17:52:31 +0530

    Creating DiaryApiAction based on DiaryAction updated

--- src/main/java/org/ideoholic/curium/model/diary/action/DiaryApiAction.java
+++ src/main/java/org/ideoholic/curium/model/diary/action/DiaryApiAction.java
@@ -5,91 +5,37 @@ import org.ideoholic.curium.model.diary.dto.AddDiaryDto;
 import org.ideoholic.curium.model.diary.dto.DairyIdsDto;
 import org.ideoholic.curium.model.diary.dto.DiaryResponseDto;
 import org.ideoholic.curium.model.diary.dto.ViewDetailsOfDiaryMessageResponseDto;
-import org.ideoholic.curium.model.diary.service.DiaryService;
-import org.ideoholic.curium.model.std.service.StandardService;
 import org.ideoholic.curium.model.student.dto.StudentIdDto;
 import org.ideoholic.curium.model.student.dto.StudentIdPageDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-@RequestMapping("/api/v1/DiaryProcess")
-public class DiaryApiAction {
-	@Autowired
-	private DiaryService diaryService;
-	@Autowired
-	private StandardService standardService;
+public interface DiaryApiAction {
 
-	@GetMapping("/getdiarystudent")
-	public ResponseEntity<ResultResponse> getdiarystudent(@RequestHeader(value = "branchid") String branchId) {
-		ResultResponse result = standardService.viewClasses(branchId);
-		return ResponseEntity.ok(result);
+	 ResponseEntity<ResultResponse> getdiarystudent(@RequestHeader(value = "branchid") String branchId) ;
 
-	}
-
-	@PostMapping("/addDiary")
-	public ResponseEntity addDiary(@RequestBody AddDiaryDto addDiaryDto,
+	 ResponseEntity addDiary(@RequestBody AddDiaryDto addDiaryDto,
 			@RequestHeader(value = "branchid") String branchId,
 			@RequestHeader(value = "currentAcademicYear") String currentAcademicYear,
-			@RequestHeader(value = "userloginid") String userLoginId) {
-		diaryService.addDiary(addDiaryDto, branchId, userLoginId, currentAcademicYear);
-		return ResponseEntity.ok().build();
+			@RequestHeader(value = "userloginid") String userLoginId);
 
-	}
+	 ResponseEntity<DiaryResponseDto> viewdiarystudent(@RequestHeader(value = "branchid") String branchId, @RequestParam(value="page")
+			String page) ;
 
-	@RequestMapping(value = "/viewdiarystudent", method = { RequestMethod.GET, RequestMethod.POST })
-	public ResponseEntity<DiaryResponseDto> viewdiarystudent(@RequestHeader(value = "branchid") String branchId, @RequestParam(value="page")
-			String page) {
-		DiaryResponseDto result = diaryService.viewDiary(branchId, page);
-		return ResponseEntity.ok(result);
-	}
+	 ResponseEntity<DiaryResponseDto> viewDiaryStudentParent(@RequestBody StudentIdPageDto studentIdPageDto,
+			@RequestHeader(value = "branchid") String branchId);
 
-	@RequestMapping(value = "/viewDiaryStudentParent", method = { RequestMethod.GET, RequestMethod.POST })
-	public ResponseEntity<DiaryResponseDto> viewDiaryStudentParent(@RequestBody StudentIdPageDto studentIdPageDto,
-			@RequestHeader(value = "branchid") String branchId) {
-		DiaryResponseDto result = diaryService.viewDiaryParent(studentIdPageDto, branchId);
-		return ResponseEntity.ok(result);
-	}
+	 ResponseEntity<DiaryResponseDto> deleteRecord(@RequestBody DairyIdsDto dairyIdsDto,
+			@RequestHeader(value = "branchid") String branchId) ;
 
-	@PostMapping("/deleteRecord")
-	public ResponseEntity<DiaryResponseDto> deleteRecord(@RequestBody DairyIdsDto dairyIdsDto,
-			@RequestHeader(value = "branchid") String branchId) {
-		diaryService.deleteRecord(dairyIdsDto);
-		DiaryResponseDto result = diaryService.viewDiary(branchId, dairyIdsDto.getPage());
-		return ResponseEntity.ok(result);
-	}
-
-	@PostMapping("/diarySaved")
-	public ResponseEntity<String> diarySaved() {
+     ResponseEntity<String> diarySaved(); 
 		
-		return ResponseEntity.ok("viewDiary");
-	}
 
-	@PostMapping("/ViewDiaryDetails")
-	public ResponseEntity<ViewDetailsOfDiaryMessageResponseDto> ViewDiaryDetails(
-			@RequestBody StudentIdDto studentIdDto) {
-		ViewDetailsOfDiaryMessageResponseDto result = diaryService.viewDetailsOfDiaryMessage(studentIdDto);
-		if (result.isSuccess()) {
+	 ResponseEntity<ViewDetailsOfDiaryMessageResponseDto> ViewDiaryDetails(
+			@RequestBody StudentIdDto studentIdDto) ;
 
-		}
-		return ResponseEntity.ok(result);
-	}
-
-	@PostMapping("/ViewDiaryDetailsParent")
-	public ResponseEntity<ViewDetailsOfDiaryMessageResponseDto> ViewDiaryDetailsParent(
-			@RequestBody StudentIdDto studentIdDto) {
-		ViewDetailsOfDiaryMessageResponseDto result = diaryService.viewDetailsOfDiaryMessage(studentIdDto);
-		if (result.isSuccess()) {
-
-		}
-		return ResponseEntity.ok(result);
-	}
+	 ResponseEntity<ViewDetailsOfDiaryMessageResponseDto> ViewDiaryDetailsParent(
+			@RequestBody StudentIdDto studentIdDto) ;
 }
