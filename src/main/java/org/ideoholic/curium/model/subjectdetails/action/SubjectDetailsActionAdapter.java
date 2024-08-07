7dc07e5e18d9857af8a789edd4ba72d65727a562
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-08-07 11:08:27 +0530

    Migrated SubjectDetails deleteMuiltiple using Adapter

--- src/main/java/org/ideoholic/curium/model/subjectdetails/action/SubjectDetailsActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/subjectdetails/action/SubjectDetailsActionAdapter.java
@@ -1,7 +1,9 @@
 package org.ideoholic.curium.model.subjectdetails.action;
 
 import org.ideoholic.curium.dto.ResultResponse;
+import org.ideoholic.curium.model.examdetails.dto.ExamIdsDto;
 import org.ideoholic.curium.model.subjectdetails.dto.SubjectDto;
+import org.ideoholic.curium.model.subjectdetails.dto.SubjectIdsDto;
 import org.ideoholic.curium.model.subjectdetails.dto.SubjectsResponseDto;
 import org.ideoholic.curium.model.subjectdetails.service.SubjectDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +44,16 @@ public class SubjectDetailsActionAdapter {
         ResultResponse result = subjectDetailsService.addSubject(subjectDto, httpSession.getAttribute("branchid").toString(),
                                                                    httpSession.getAttribute("userloginid").toString());
         return result.isSuccess();
+    }
+    public boolean deleteMultiple() {
+        SubjectDetailsService subjectDetailsService = new SubjectDetailsService(request,response);
+
+        SubjectIdsDto subjectIdsDto = new SubjectIdsDto();
+        subjectIdsDto.setSubjectIds(request.getParameterValues("subjectIDs"));
+
+        ResultResponse resultResponse = subjectDetailsService.deleteMultiple(subjectIdsDto);
+
+        return resultResponse.isSuccess();
     }
 
 }
