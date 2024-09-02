db291d1074f42ea566866aa640204394ebd9871a
Md Shahnawaz Ahmad <shahnawazahmad1050@gmail.com>
2024-09-02 10:59:45 +0530

    Updated SubjectsExamsResponseDto file.

--- src/main/java/org/ideoholic/curium/model/subjectdetails/dto/SubjectsExamsResponseDto.java
+++ src/main/java/org/ideoholic/curium/model/subjectdetails/dto/SubjectsExamsResponseDto.java
@@ -3,7 +3,6 @@ package org.ideoholic.curium.model.subjectdetails.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import org.ideoholic.curium.model.examdetails.dto.Exams;
-import org.ideoholic.curium.model.examdetails.dto.Examschedule;
 import org.ideoholic.curium.model.std.dto.Classsec;
 import java.util.List;
 
