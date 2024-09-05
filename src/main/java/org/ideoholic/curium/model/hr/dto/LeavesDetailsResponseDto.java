270ac44969c1e13e83aa9cde2fdc44e72cb276be
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-09-05 20:08:08 +0530

    Migrated HR viewLeavesDetails using Adapter

---
+++ src/main/java/org/ideoholic/curium/model/hr/dto/LeavesDetailsResponseDto.java
@@ -0,0 +1,21 @@
+package org.ideoholic.curium.model.hr.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.ideoholic.curium.model.academicyear.dto.Currentacademicyear;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
+
+public class LeavesDetailsResponseDto {
+
+    private Currentacademicyear currentAcademicYear;
+    private List<Leavedetails> leaveDetailsList;
+    private String teacherName;
+    private String leaveDetailsTeachersId;
+    private String academicPerYear;
+    private boolean success;
+
+}
