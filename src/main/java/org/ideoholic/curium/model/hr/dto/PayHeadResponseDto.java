7eb689aa8982963cc57e7a3bcf721576cb863822
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-09-06 21:12:35 +0530

    Migrated  HR payHead using Adapter

---
+++ src/main/java/org/ideoholic/curium/model/hr/dto/PayHeadResponseDto.java
@@ -0,0 +1,12 @@
+package org.ideoholic.curium.model.hr.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
+public class PayHeadResponseDto {
+    private List<Payhead> payHeadList;
+}
