2aac87a6779c3be646cefa4de7571f51f751eb4e
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-10-13 12:16:40 +0530

    Migrated HR getStaffDetails using Adapter (#676)

---
+++ src/main/java/org/ideoholic/curium/model/hr/dto/StaffDetailsResponseDto.java
@@ -0,0 +1,14 @@
+package org.ideoholic.curium.model.hr.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
+public class StaffDetailsResponseDto {
+
+    private List<Payheadstaffdetails> payHeadDetailsList;
+
+}
