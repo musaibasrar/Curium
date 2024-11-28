c7d460e2d0a28ece30c2830148b4357275493885
Shaik Shafeeq Ahmed <144222899+shafeeq-cse@users.noreply.github.com>
2024-11-28 09:55:31 +0530

    Migrating PeriodAction viewTeacherTimeTable using Adapter.

---
+++ src/main/java/org/ideoholic/curium/model/periods/dto/TeacherTimeTableResponseDto.java
@@ -0,0 +1,17 @@
+package org.ideoholic.curium.model.periods.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
+public class TeacherTimeTableResponseDto {
+    private String teacherName;
+    private List<Map> periodMapList;
+    private boolean success;
+}
