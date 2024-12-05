71874715193925d52d117d2010cf8621ce015391
Shaik Shafeeq Ahmed <shafeeq7.cse@gmail.com>
2024-12-05 17:26:49 +0530

    Migrating UserAction authenticateUser using Adapter.

---
+++ src/main/java/org/ideoholic/curium/model/user/dto/UserAuthenticationDto.java
@@ -0,0 +1,15 @@
+package org.ideoholic.curium.model.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserAuthenticationDto {
+    private String userName;
+    private String password;
+}
---
+++ src/main/java/org/ideoholic/curium/model/user/dto/UserAuthenticationResponseDto.java
@@ -0,0 +1,25 @@
+package org.ideoholic.curium.model.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
+public class UserAuthenticationResponseDto {
+    private String academicYear;
+    private String userName;
+    private Integer branchId;
+    private String branchName;
+    private String branchCode;
+    private String branchAddress;
+    private String branchContact;
+    private String userType;
+    private String typeOfUser;
+    private String userAuth;
+    private Integer userLoginId;
+    private String attendanceStatus;
+    @Builder.Default
+    private boolean success = false;
+}
