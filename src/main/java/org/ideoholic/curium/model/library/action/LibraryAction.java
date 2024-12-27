529e092809ebb84856bdbf454ead9358105ac528
Shaik Shafeeq Ahmed <144222899+shafeeq-cse@users.noreply.github.com>
2024-12-27 10:40:57 +0530

    Creating AttendanceApiAction and AttendanceApiActionImpl

--- src/main/java/org/ideoholic/curium/model/library/action/LibraryAction.java
+++ src/main/java/org/ideoholic/curium/model/library/action/LibraryAction.java
@@ -1,10 +1,6 @@
 package org.ideoholic.curium.model.library.action;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.ideoholic.curium.model.documents.action.DocumentActionAdapter;
-import org.ideoholic.curium.model.library.service.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,107 +8,111 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 @Controller
 @RequestMapping("/LibraryProcess")
 public class LibraryAction {
-	@Autowired
-	private HttpServletRequest request;
-	
-	@Autowired
-	private HttpServletResponse response;
-	
-	@Autowired
-	private LibraryActionAdapter libraryActionAdapter;
-	
-	@Autowired
-	private DocumentActionAdapter documentActionAdapter;
-	
-	public String error ="error";
+    @Autowired
+    private HttpServletRequest request;
 
-	
-	@GetMapping("/addbooks")
-	public String addBooks() {
-		return "addbook";
-	}
-	
-	@PostMapping("/saveBook")
-	public String saveBook() {
-		libraryActionAdapter.addBook();
-		return "bookSave";
+    @Autowired
+    private HttpServletResponse response;
 
-	}
-	
-	@RequestMapping(value = "/viewbooks", method = { RequestMethod.GET, RequestMethod.POST })
-	public String viewbooks() {
-		libraryActionAdapter.viewBooks();
-		return "Viewbook";
+    @Autowired
+    private LibraryActionAdapter libraryActionAdapter;
 
-	}
-	
-	@PostMapping("/deleteRecord")
-	public String deleteRecord() {
-		libraryActionAdapter.deleteRecord();
-		libraryActionAdapter.viewBooks();
-		return "Viewbook";
-	}
-	
-	@RequestMapping(value = "/issuebooks", method = { RequestMethod.GET, RequestMethod.POST })
-	public String issuebooks() {
-		if (documentActionAdapter.transferCertificate()) {
-			libraryActionAdapter.viewBooksAvailable();
-			return "issuebook";
-		}
-		return error;
-	}
-	
-	@PostMapping("/bookIssuedStudent")
-	public String bookIssuedStudent() {
-		libraryActionAdapter.updateBook();
-		return "bookIssued";
+    @Autowired
+    private DocumentActionAdapter documentActionAdapter;
 
-	}
-	
-	@GetMapping("/returnbooks")
-	public String bookReturnStudent() {
-		documentActionAdapter.transferCertificate();
-		return "bookReturn";
+    public String error ="error";
 
-	}
-	
-	@PostMapping("/searchbooks")
-	public String searchbooks() {
-		libraryActionAdapter.searchStudentBook();
-		return "bookReturn";
 
-	}
-	
-	@PostMapping("/bookReturnByStudent")
-	public String bookReturnByStudent() {
-		libraryActionAdapter.bookReturnByStudent();
-		return "bookReturnedSuccessfully";
+    @GetMapping("/addbooks")
+    public String addBooks() {
+        return "addbook";
+    }
 
-	}
-	
-	@GetMapping("/bookdetail")
-	public String bookdetail() {
-		libraryActionAdapter.viewBookdetails();
-		return "book_details";
-	}
-	
-	@PostMapping("/updateBookDetails")
-	public String updateEmployeeDetails() {
-		if (libraryActionAdapter.viewBookdetails()) {
-			return "book_update";
-		} else {
-			return "viewAll";
-		}
-	}
-	
-	@PostMapping("/updateBook")
-	public String updateBook() {
+    @PostMapping("/saveBook")
+    public String saveBook() {
+        libraryActionAdapter.addBook();
+        return "bookSave";
 
-		libraryActionAdapter.updateBookitems();
-		return viewbooks();
-	}
+    }
+
+    @RequestMapping(value = "/viewbooks", method = { RequestMethod.GET, RequestMethod.POST })
+    public String viewbooks() {
+        libraryActionAdapter.viewBooks();
+        return "Viewbook";
+
+    }
+
+    @PostMapping("/deleteRecord")
+    public String deleteRecord() {
+        libraryActionAdapter.deleteRecord();
+        libraryActionAdapter.viewBooks();
+        return "Viewbook";
+    }
+
+    @RequestMapping(value = "/issuebooks", method = { RequestMethod.GET, RequestMethod.POST })
+    public String issuebooks() {
+        if (documentActionAdapter.transferCertificate()) {
+            libraryActionAdapter.viewBooksAvailable();
+            return "issuebook";
+        }
+        return error;
+    }
+
+    @PostMapping("/bookIssuedStudent")
+    public String bookIssuedStudent() {
+        libraryActionAdapter.updateBook();
+        return "bookIssued";
+
+    }
+
+    @GetMapping("/returnbooks")
+    public String bookReturnStudent() {
+        documentActionAdapter.transferCertificate();
+        return "bookReturn";
+
+    }
+
+    @PostMapping("/searchbooks")
+    public String searchbooks() {
+        libraryActionAdapter.searchStudentBook();
+        return "bookReturn";
+
+    }
+
+    @PostMapping("/bookReturnByStudent")
+    public String bookReturnByStudent() {
+        libraryActionAdapter.bookReturnByStudent();
+        return "bookReturnedSuccessfully";
+
+    }
+
+    @GetMapping("/bookdetail")
+    public String bookdetail() {
+        libraryActionAdapter.viewBookdetails();
+        return "book_details";
+    }
+
+    @PostMapping("/updateBookDetails")
+    public String updateEmployeeDetails() {
+        if (libraryActionAdapter.viewBookdetails()) {
+            return "book_update";
+        } else {
+            return "viewAll";
+        }
+    }
+
+    @PostMapping("/updateBook")
+    public String updateBook() {
+
+        libraryActionAdapter.updateBookitems();
+        return viewbooks();
+    }
 
 }
