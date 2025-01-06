b3262fb214bfb46347f4e64e819096ef0d3cecb6
Tauqueer <mdtauqueeralam031@gmail.com>
2025-01-06 09:46:29 +0530

    updated

--- src/main/java/org/ideoholic/curium/model/studentdiary/action/StudentDiaryApiActionImpl.java
+++ src/main/java/org/ideoholic/curium/model/studentdiary/action/StudentDiaryApiActionImpl.java
@@ -14,92 +14,74 @@ import org.ideoholic.curium.model.studentdiary.dto.AddStudentDiaryDto;
 import org.ideoholic.curium.model.studentdiary.service.StudentDiaryservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentDiaryApiActionImpl implements StudentDiaryApiAction {
 	@Autowired
 	private DiaryService diaryService;
-    @Autowired
-    private StudentDiaryservice studentDiaryservice;
-    @Autowired
-    private StudentService studentService;
-	
-	public ResponseEntity<ParentListResponseDto> getdiarystudent(@RequestParam(value="page")
-	String page,@RequestHeader(value = "branchid") String branchId) {
-		ParentListResponseDto result = studentService.viewAllStudentsParents(page,branchId);
+	@Autowired
+	private StudentDiaryservice studentDiaryservice;
+	@Autowired
+	private StudentService studentService;
+
+	public ResponseEntity<ParentListResponseDto> getdiarystudent(String page, String branchId) {
+		ParentListResponseDto result = studentService.viewAllStudentsParents(page, branchId);
 		return ResponseEntity.ok(result);
 	}
-	
-	public ResponseEntity addDiary(@RequestBody AddStudentDiaryDto addStudentDiaryDto,
-			@RequestHeader(value = "branchid") String branchId,
-			@RequestHeader(value = "currentAcademicYear") String currentAcademicYear,
-			@RequestHeader(value = "userloginid") String userLoginId) {
+
+	public ResponseEntity addDiary(AddStudentDiaryDto addStudentDiaryDto, String branchId, String currentAcademicYear,
+			String userLoginId) {
 		studentDiaryservice.addDiary(addStudentDiaryDto, branchId, userLoginId, currentAcademicYear);
 		return ResponseEntity.ok().build();
 
 	}
-	
-	
-	public ResponseEntity<DiaryResponseDto> viewdiarystudent(@RequestHeader(value = "branchid") String branchId, @RequestParam(value="page")
-	String page) {
+
+	public ResponseEntity<DiaryResponseDto> viewdiarystudent(String branchId, String page) {
 		DiaryResponseDto result = studentDiaryservice.viewDiary(branchId, page);
 		return ResponseEntity.ok(result);
 	}
-	
-	  public ResponseEntity<DiaryResponseDto> viewDiaryStudentParentGet(@RequestParam(value="page") String page, @RequestParam(value="studentId") String studentId,
-	      @RequestHeader(value = "branchid") String branchId) {
-	        StudentIdPageDto studentIdPageDto = new StudentIdPageDto();
-	        studentIdPageDto.setPage(page);
-	        studentIdPageDto.setStudentId(studentId);
-	        
-	    DiaryResponseDto result = studentDiaryservice.viewDiaryParent(studentIdPageDto, branchId);
-	    return ResponseEntity.ok(result);
-	  }
 
-	  public ResponseEntity<DiaryResponseDto> viewDiaryStudentParentPost(@RequestBody StudentIdPageDto studentIdPageDto,
-	      @RequestHeader(value = "branchid") String branchId) {
-	    DiaryResponseDto result = studentDiaryservice.viewDiaryParent(studentIdPageDto, branchId);
-	    return ResponseEntity.ok(result);
-	  }
-	
-	public ResponseEntity<DiaryResponseDto>  deleteRecord(@RequestBody DairyIdsDto dairyIdsDto,@RequestParam(value="page")
-	String page,
-	@RequestHeader(value = "branchid") String branchId) {
-		studentDiaryservice.deleteRecord(dairyIdsDto);
-		DiaryResponseDto result = studentDiaryservice.viewDiary(page,branchId);
+	public ResponseEntity<DiaryResponseDto> viewDiaryStudentParentGet(String page, String studentId, String branchId) {
+		StudentIdPageDto studentIdPageDto = new StudentIdPageDto();
+		studentIdPageDto.setPage(page);
+		studentIdPageDto.setStudentId(studentId);
+
+		DiaryResponseDto result = studentDiaryservice.viewDiaryParent(studentIdPageDto, branchId);
 		return ResponseEntity.ok(result);
 	}
-	
+
+	public ResponseEntity<DiaryResponseDto> viewDiaryStudentParentPost(StudentIdPageDto studentIdPageDto,
+			String branchId) {
+		DiaryResponseDto result = studentDiaryservice.viewDiaryParent(studentIdPageDto, branchId);
+		return ResponseEntity.ok(result);
+	}
+
+	public ResponseEntity<DiaryResponseDto> deleteRecord(DairyIdsDto dairyIdsDto, String page, String branchId) {
+		studentDiaryservice.deleteRecord(dairyIdsDto);
+		DiaryResponseDto result = studentDiaryservice.viewDiary(page, branchId);
+		return ResponseEntity.ok(result);
+	}
+
 	public ResponseEntity<String> diarySaved() {
 		return ResponseEntity.ok("viewdiarystudent");
 	}
-	
-	public ResponseEntity<DiaryDetailsMessageResponseDto> ViewDiaryDetails(@RequestBody StudentIdDto studentIdDto) {
+
+	public ResponseEntity<DiaryDetailsMessageResponseDto> ViewDiaryDetails(StudentIdDto studentIdDto) {
 		DiaryDetailsMessageResponseDto result = diaryService.viewDetailsOfDiaryMessage(studentIdDto);
-		if(result.isSuccess()) {
+		if (result.isSuccess()) {
+			return ResponseEntity.ok(result);
+		} else {
+			throw new CustomResponseException(CustomErrorMessage.ERROR);
+		}
+	}
+
+	public ResponseEntity<DiaryDetailsMessageResponseDto> ViewDiaryDetailsParent(StudentIdDto studentIdDto) {
+		DiaryDetailsMessageResponseDto result = diaryService.viewDetailsOfDiaryMessage(studentIdDto);
+		if (result.isSuccess()) {
 			return ResponseEntity.ok(result);
 		}
-		else {
 		throw new CustomResponseException(CustomErrorMessage.ERROR);
-		}
-		}
-	
-	public ResponseEntity<DiaryDetailsMessageResponseDto> ViewDiaryDetailsParent(@RequestBody StudentIdDto studentIdDto) {
-		DiaryDetailsMessageResponseDto result = diaryService.viewDetailsOfDiaryMessage(studentIdDto);
-		if(result.isSuccess()) { 
-			return ResponseEntity.ok(result);
-		}
-		throw new CustomResponseException(CustomErrorMessage.ERROR);
-		}
+	}
 
-		}
-
+}
