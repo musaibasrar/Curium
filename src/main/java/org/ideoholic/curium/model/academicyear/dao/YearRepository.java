06c508288f3b290c09d07be25fe8f1621b5502c8
Tauqueer <mdtauqueeralam031@gmail.com>
2025-02-21 09:52:14 +0530

    updated

--- src/main/java/org/ideoholic/curium/model/academicyear/dao/YearRepository.java
+++ src/main/java/org/ideoholic/curium/model/academicyear/dao/YearRepository.java
@@ -6,6 +6,5 @@ import org.ideoholic.curium.model.academicyear.dto.Currentacademicyear;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface YearRepository extends JpaRepository<Currentacademicyear, Integer>{
-	List<Currentacademicyear> findByCurrentacademicyear(String currentacademicyear);
 
 }
