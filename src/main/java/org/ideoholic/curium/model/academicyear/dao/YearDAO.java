435c0b409d614000d97d2d973661a7f7e6654997
Tauqueer <mdtauqueeralam031@gmail.com>
2025-02-22 09:01:40 +0530

    updated

--- src/main/java/org/ideoholic/curium/model/academicyear/dao/YearDAO.java
+++ src/main/java/org/ideoholic/curium/model/academicyear/dao/YearDAO.java
@@ -1,24 +1,16 @@
 package org.ideoholic.curium.model.academicyear.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import org.ideoholic.curium.util.Session;
-import org.ideoholic.curium.util.Session.Transaction;
+import org.ideoholic.curium.model.academicyear.dto.Currentacademicyear;
+import org.ideoholic.curium.util.QueryUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import lombok.extern.slf4j.Slf4j;
-
-import org.hibernate.query.Query;
-
-import org.ideoholic.curium.model.academicyear.dto.Currentacademicyear;
-import org.ideoholic.curium.model.user.dao.LoginRepository;
-import org.ideoholic.curium.model.user.dao.UserDAO;
-import org.ideoholic.curium.util.HibernateUtil;
-import org.ideoholic.curium.util.QueryUtil;
 
 @Slf4j
 @Component
@@ -50,19 +42,19 @@ public class YearDAO {
 	}
 
     @Transactional
-	public Currentacademicyear showYear() {
+    public Currentacademicyear showYear() {
 		Currentacademicyear currentacademicyear = new Currentacademicyear();
 		try {
-			 Session session = HibernateUtil.openCurrentSession();
-			Query query = session.createQuery(
-					"from Currentacademicyear as ca where ca.cayid = (select max(cayid) from Currentacademicyear) ");
-			currentacademicyear = (Currentacademicyear) query.setCacheable(true).setCacheRegion("commonregion")
-					.uniqueResult();
-		} catch (Exception hibernateException) {
-			 log.error(hibernateException.getMessage(), hibernateException);
-	            hibernateException.printStackTrace();
-	            throw hibernateException;
-	            } 
+			
+			List<Currentacademicyear> ca = queryUtil.runGivenQuery("select ca from Currentacademicyear as ca where ca.cayid = (select max(cayid) from Currentacademicyear) ", Currentacademicyear.class);
+			if(!CollectionUtils.isEmpty(ca)) {
+				currentacademicyear = ca.get(0);
+			}
+		} catch (Exception hibernateException) { 
+			log.error(hibernateException.getMessage(), hibernateException);
+			hibernateException.printStackTrace();
+			throw hibernateException;
+		} 
 		return currentacademicyear;
 	}
 
