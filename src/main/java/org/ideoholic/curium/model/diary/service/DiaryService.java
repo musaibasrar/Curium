171265e6572df873329596bd9b484f6feb2c3b02
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2025-03-01 14:55:25 +0530

    Migrate diaryDAO readListOfObjects to use JPA repository

--- src/main/java/org/ideoholic/curium/model/diary/service/DiaryService.java
+++ src/main/java/org/ideoholic/curium/model/diary/service/DiaryService.java
@@ -68,25 +68,8 @@ public class DiaryService {
 				if (!"".equalsIgnoreCase(DataUtil.emptyString(strPage))) {
 					page = Integer.parseInt(strPage);
 				}
-				List<Object[]> list = diarysDao.readListOfObjects((page - 1) * recordsPerPage,
+				List<Diary> diaryDetails = diarysDao.readListOfObjects((page - 1) * recordsPerPage,
 						recordsPerPage, Integer.parseInt(branchId));
-
-				List<Diary> diaryDetails = new ArrayList<Diary>();
-				for (Object[] diaryObject : list) {
-					Diary diary = new Diary();
-
-					diary.setId((Integer) diaryObject[0]);
-					diary.setClasssec((String) diaryObject[1]);
-					diary.setAcademicyear((String) diaryObject[2]);
-					diary.setBranchid(Integer.parseInt((String) diaryObject[3]));
-					diary.setSubject((String) diaryObject[4]);
-					diary.setMessage((String) diaryObject[5]);
-					diary.setStartdate((Date) diaryObject[6]);
-					diary.setEnddate((Date) diaryObject[7]);
-					diary.setCreateddate((Date) diaryObject[8]);
-					diaryDetails.add(diary);
-				}
-
 				int noOfRecords = diarysDao.getNoOfRecords(Integer.parseInt(branchId));
 				int noOfPages = (int) Math.ceil(noOfRecords * 1.0 / recordsPerPage);
 				diaryResponseDto.setDiary(diaryDetails);
