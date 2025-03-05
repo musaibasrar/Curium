15eb93de03b5bd80ea70ac0d3be22eceeff6bb13
Md Shahnawaz Ahmad <shahnawazahmad1050@gmail.com>
2025-03-05 10:17:32 +0530

    Migrated departmentDAO readListOfObjects using SpringJPA

--- src/main/java/org/ideoholic/curium/model/department/dao/DepartmentRepository.java
+++ src/main/java/org/ideoholic/curium/model/department/dao/DepartmentRepository.java
@@ -2,9 +2,13 @@ package org.ideoholic.curium.model.department.dao;
 
 import org.ideoholic.curium.model.department.dto.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department,Integer> {
-
+    @Query("From Department where branchid=:branchId")
+    List<Department> finadAllDepartment(int branchId);
 }
