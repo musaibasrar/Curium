69714e0be0ff99752ebe461b7dbd19452e6a6c93
Mushy <musaibasrar@gmail.com>
2025-03-17 01:22:55 +0530

    View Next academic Year Other Fees
    (cherry picked from commit 04c9b4c0a31bd53ccb4e0081601a512a5008190c)

---
+++ src/main/java/org/ideoholic/curium/model/feescollection/dto/OtherFeesDetailsResponseDto.java
@@ -0,0 +1,30 @@
+package org.ideoholic.curium.model.feescollection.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.ideoholic.curium.model.student.dto.Studentfeesstructure;
+import org.ideoholic.curium.model.student.dto.Studentotherfeesstructure;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
+public class OtherFeesDetailsResponseDto {
+    private List<Otherreceiptinfo> receiptInfo;
+    private List<Studentfeesstructure> feesStructure;
+    private long totalSum;
+    private long totalFeesAmount;
+    private long dueAmount;
+    private long totalFeesConcession;
+    private String academicPerYear;
+    private String currentAcademicYear;
+    private List<Studentotherfeesstructure> otherFeesStructure;
+    @Builder.Default
+    private boolean success = false;
+}
