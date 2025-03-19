0f22c67c3ecd0660871fbc9d0fae0343c2b069f8
Tauqueer <mdtauqueeralam031@gmail.com>
2025-03-19 10:13:30 +0530

    account detail updated

--- src/main/java/org/ideoholic/curium/model/account/dao/AccountGroupMasterRepository.java
+++ src/main/java/org/ideoholic/curium/model/account/dao/AccountGroupMasterRepository.java
@@ -2,6 +2,8 @@ package org.ideoholic.curium.model.account.dao;
 
 import org.ideoholic.curium.model.account.dto.Accountgroupmaster;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountGroupMasterRepository extends JpaRepository<Accountgroupmaster, Integer>{
 }
