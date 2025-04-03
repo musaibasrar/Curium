ced687f8dc515eb40b48f31d691ea560a4579e63
Tauqueer <mdtauqueeralam031@gmail.com>
2025-04-03 09:25:32 +0530

    Migrate AccountDAO getVoucherEntryTransactions to use JPA repository

--- src/main/java/org/ideoholic/curium/model/account/dao/VoucherEntryTransactionsRepository.java
+++ src/main/java/org/ideoholic/curium/model/account/dao/VoucherEntryTransactionsRepository.java
@@ -1,8 +1,14 @@
 package org.ideoholic.curium.model.account.dao;
 
+import java.util.List;
+
 import org.ideoholic.curium.model.account.dto.VoucherEntrytransactions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface VoucherEntryTransactionsRepository extends JpaRepository<VoucherEntrytransactions, Integer> {
+	
+	 @Query("FROM VoucherEntrytransactions v WHERE v.financialyear = :financialYear AND v.cancelvoucher = 'yes' AND v.branchid = :branchId ORDER BY v.transactionsid ASC")
+	    List<VoucherEntrytransactions> findCancelledVoucherEntryTransactions(Integer financialYear, int branchId);
 
 }
