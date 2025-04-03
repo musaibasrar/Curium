55222b28f06f740a5e7d538c034b7cc397021166
Tauqueer <mdtauqueeralam031@gmail.com>
2025-04-03 17:40:33 +0530

    updated

--- src/main/java/org/ideoholic/curium/model/account/dao/VoucherEntryTransactionsRepository.java
+++ src/main/java/org/ideoholic/curium/model/account/dao/VoucherEntryTransactionsRepository.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.ideoholic.curium.model.account.dto.VoucherEntrytransactions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VoucherEntryTransactionsRepository extends JpaRepository<VoucherEntrytransactions, Integer> {
 	
 	 @Query("FROM VoucherEntrytransactions v WHERE v.financialyear = :financialYear AND v.cancelvoucher = 'yes' AND v.branchid = :branchId ORDER BY v.transactionsid ASC")
-	    List<VoucherEntrytransactions> findCancelledVoucherEntryTransactions(Integer financialYear, int branchId);
+	 List<VoucherEntrytransactions> findCancelledVoucherEntryTransactions(@Param("financialYear")Integer financialYear, @Param("branchId")Integer branchId);
 
 }
