9fb1cab070f5f189b0c8587a46ef8673610d5a34
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2025-04-11 17:39:35 +0530

    Migrated AppointmentDAO readListOfObjectsPagination using SpringJPA

--- src/main/java/org/ideoholic/curium/model/appointment/dao/AppointmentRepository.java
+++ src/main/java/org/ideoholic/curium/model/appointment/dao/AppointmentRepository.java
@@ -3,10 +3,14 @@ package org.ideoholic.curium.model.appointment.dao;
 import java.util.List;
 
 import org.ideoholic.curium.model.appointment.dto.Appointment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
 	List<Appointment> findByBranchidOrderByIdDesc(int branchid);
+
+	Page<Appointment> findByBranchidOrderByIdDesc(int branchId, Pageable pageable);
 }
