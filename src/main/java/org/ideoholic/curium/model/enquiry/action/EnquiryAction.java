9cc0a3ac1d791575c540c9e15c5b79f23e29cb16
Tauqueer <mdtauqueeralam031@gmail.com>
2025-04-17 12:55:04 +0530

    Admission enquiry For vision
    (cherry picked from commit 89c4563c2e097e0f4438968c23d11b6c5017b2d5)

--- src/main/java/org/ideoholic/curium/model/enquiry/action/EnquiryAction.java
+++ src/main/java/org/ideoholic/curium/model/enquiry/action/EnquiryAction.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/EnquiryProcess")
@@ -28,15 +29,66 @@ public class EnquiryAction {
 	@Autowired
 	private EnquiryActionAdapter enquiryActionAdapter;
 	
-	@GetMapping("/newEnquiry")
-	public String NewEnquiryDetail() {
-		standardActionAdapter.viewClasses();
-		return "newenquiry";
-	}
+	/*
+	 * @GetMapping("/newEnquiry") public String NewEnquiryDetail() {
+	 * standardActionAdapter.viewClasses(); return "newenquiry"; }
+	 * 
+	 * @PostMapping("/genarateNewCertificate") public String
+	 * genarateNewCertificate() { enquiryActionAdapter.getCertificate(); return
+	 * "newcertificatepreview"; }
+	 */
 
-	@PostMapping("/genarateNewCertificate")
-	public String genarateNewCertificate() {
-		enquiryActionAdapter.getCertificate();
-		return "newcertificatepreview";
+		
+		
+	@GetMapping("/enquiry")
+	public String enquiry() {
+		standardActionAdapter.viewClasses();
+		return "enquiryform";
 	}
+	
+	@PostMapping("/saveEnquiryForm")
+	public String saveEnquiryForm() {
+		if(enquiryActionAdapter.saveEnquiryForm()) {
+			enquiryActionAdapter.getStudentLastEnquiry();
+			return "studentenquiryformprint";
+		}else {
+			return "error";
+		}
+		
 	}
+	
+	@RequestMapping(value = "/viewEnquiry", method = { RequestMethod.GET, RequestMethod.POST })
+	public String viewEnquiry() {
+		enquiryActionAdapter.viewEnquiry(); 
+		return "viewenquirylist";
+	}
+	
+	@GetMapping("/getStudentEnquiryform")
+	public String getStudentEnquiryform() {
+		enquiryActionAdapter.getStudentEnquiry(); 
+		return "studentenquiryformprint";
+	}
+	
+	@PostMapping("/updateEnquiryDetails")
+	public String updateEmployeeDetails() {
+		enquiryActionAdapter.getStudentEnquiry(); 
+			return "enquiry_update";
+	}
+	
+	@PostMapping("/updateEnquiryFormDetails")
+	public String updateEnquiryFormDetails() {
+		if(enquiryActionAdapter.updateEnquiry()) {
+			standardActionAdapter.viewClasses();
+			return "updatesuccessful";	
+		}
+		return "error";
+	}
+	
+	@PostMapping("/deleteEnquiry")
+	public String deleteEnquiry() {
+		enquiryActionAdapter.deleteEnquiry(); 
+			return viewEnquiry();
+	}
+	
+
+}
