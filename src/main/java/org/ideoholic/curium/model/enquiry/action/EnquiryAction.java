0e159a89da52195d2aaf82f8516492b6533f66b9
Tauqueer <mdtauqueeralam031@gmail.com>
2025-04-18 17:22:28 +0530

    added lombok, branchid and userid in admission enquiry in vision branchid
    (cherry picked from commit b8c655b2dace9aedc468070221a57dbe5f5f0abc)

--- src/main/java/org/ideoholic/curium/model/enquiry/action/EnquiryAction.java
+++ src/main/java/org/ideoholic/curium/model/enquiry/action/EnquiryAction.java
@@ -1,13 +1,6 @@
 package org.ideoholic.curium.model.enquiry.action;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.ideoholic.curium.model.documents.service.DocumentService;
-import org.ideoholic.curium.model.enquiry.service.EnquiryService;
 import org.ideoholic.curium.model.std.action.StandardActionAdapter;
-import org.ideoholic.curium.model.std.service.StandardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,27 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/EnquiryProcess")
 public class EnquiryAction {
-	@Autowired
-	private HttpServletRequest request;
-	@Autowired
-	private HttpServletResponse response;
-	@Autowired
-	private HttpSession httpSession;
+	
 	@Autowired
 	private StandardActionAdapter standardActionAdapter;
 	@Autowired
 	private EnquiryActionAdapter enquiryActionAdapter;
 	
-	/*
-	 * @GetMapping("/newEnquiry") public String NewEnquiryDetail() {
-	 * standardActionAdapter.viewClasses(); return "newenquiry"; }
-	 * 
-	 * @PostMapping("/genarateNewCertificate") public String
-	 * genarateNewCertificate() { enquiryActionAdapter.getCertificate(); return
-	 * "newcertificatepreview"; }
-	 */
-
-		
 		
 	@GetMapping("/enquiry")
 	public String enquiry() {
