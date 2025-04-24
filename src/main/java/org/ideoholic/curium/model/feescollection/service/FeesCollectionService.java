621b34cd6fb5dd15f6cc8555aa6ed871b1b76829
Mushy <musaibasrar@gmail.com>
2025-04-24 14:48:49 +0530

    accounts linking with fees bug fix

--- src/main/java/org/ideoholic/curium/model/feescollection/service/FeesCollectionService.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/service/FeesCollectionService.java
@@ -342,7 +342,7 @@ public class FeesCollectionService {
 			
 			String updateDrAccount="update Accountdetailsbalance set currentbalance=currentbalance+"+onlyTotalFee+" where accountdetailsid="+drAccount;
 
-			String updateCrAccount="update Accountdetailsbalance set currentbalance=currentbalance+"+onlyTotalFee+" where accountdetailsid="+crFees;
+			String updateCrAccount="update Accountdetailsbalance set currentbalance=currentbalance-"+onlyTotalFee+" where accountdetailsid="+crFees;
 			
 			// End Receipt
 			
@@ -366,7 +366,7 @@ public class FeesCollectionService {
 			transactionsIncome.setBranchid(Integer.parseInt(branchId));
 			transactionsIncome.setUserid(Integer.parseInt(userId));
 			
-			String updateDrAccountIncome="update Accountdetailsbalance set currentbalance=currentbalance+"+onlyTotalFee+" where accountdetailsid="+drAccountIncome;
+			String updateDrAccountIncome="update Accountdetailsbalance set currentbalance=currentbalance-"+onlyTotalFee+" where accountdetailsid="+drAccountIncome;
 
 			String updateCrAccountIncome="update Accountdetailsbalance set currentbalance=currentbalance+"+onlyTotalFee+" where accountdetailsid="+crFeesIncome;
 			
@@ -2567,7 +2567,7 @@ public class FeesCollectionService {
 			
 			String updateDrAccount="update Accountdetailsbalance set currentbalance=currentbalance+"+onlyTotalFee+" where accountdetailsid="+drAccount;
 
-			String updateCrAccount="update Accountdetailsbalance set currentbalance=currentbalance+"+onlyTotalFee+" where accountdetailsid="+crFees;
+			String updateCrAccount="update Accountdetailsbalance set currentbalance=currentbalance-"+onlyTotalFee+" where accountdetailsid="+crFees;
 			
 			// End Receipt
 			
@@ -2591,7 +2591,7 @@ public class FeesCollectionService {
 			transactionsIncome.setBranchid(Integer.parseInt(branchId));
 			transactionsIncome.setUserid(Integer.parseInt(userId));
 			
-			String updateDrAccountIncome="update Accountdetailsbalance set currentbalance=currentbalance+"+onlyTotalFee+" where accountdetailsid="+drAccountIncome;
+			String updateDrAccountIncome="update Accountdetailsbalance set currentbalance=currentbalance-"+onlyTotalFee+" where accountdetailsid="+drAccountIncome;
 
 			String updateCrAccountIncome="update Accountdetailsbalance set currentbalance=currentbalance+"+onlyTotalFee+" where accountdetailsid="+crFeesIncome;
 			
