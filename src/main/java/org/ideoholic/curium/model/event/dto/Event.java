9b347214f382cd9a129e082d411897055eebdab2
Tauqueer <mdtauqueeralam031@gmail.com>
2025-04-24 11:52:25 +0530

    updated event module with branchid and userid
    (cherry picked from commit 49f870e1d9757b9e91542364ff938d5579fe625b)

--- src/main/java/org/ideoholic/curium/model/event/dto/Event.java
+++ src/main/java/org/ideoholic/curium/model/event/dto/Event.java
@@ -11,6 +11,12 @@ public class Event implements java.io.Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @Column
+    private int branchid;
+    
+    @Column
+    private int userid;
+    
     @Column(nullable = false)
     private String title;
     
@@ -49,8 +55,25 @@ public class Event implements java.io.Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+    
 
-    public String getTitle() {
+    public int getBranchid() {
+		return branchid;
+	}
+
+	public void setBranchid(int branchid) {
+		this.branchid = branchid;
+	}
+	
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getTitle() {
         return title;
     }
 
