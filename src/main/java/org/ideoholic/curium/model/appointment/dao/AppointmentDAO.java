53e9541ca565893253968776ea0d60e4fee08970
Md Shahnawaz Ahmad <shahnawazahmad1050@gmail.com>
2025-04-24 09:51:19 +0530

    Migrated AppointmentDAO getNoOfRecords using SpringJPA

--- src/main/java/org/ideoholic/curium/model/appointment/dao/AppointmentDAO.java
+++ src/main/java/org/ideoholic/curium/model/appointment/dao/AppointmentDAO.java
@@ -92,31 +92,26 @@ public class AppointmentDAO {
 				return noOfRecords;
 
 		}
-		
+		@Transactional
 		public int getNoOfRecords() {
 			
 			List<Appointment> results = new ArrayList<Appointment>();
 			int noOfRecords = 0;
-			Transaction transaction = null;
 			try{
-				Session session = HibernateUtil.openCurrentSession();
-				transaction = session.beginTransaction();
 
-				results = (List<Appointment>) session.createQuery("From Appointment where status != 'Cancelled'").setCacheable(true).setCacheRegion("commonregion")
-						.list();
-				noOfRecords = results.size();
-				log.info("The size of list is:::::::::::::::::::::::::::::::::::::::::: "
-								+ noOfRecords);
-				transaction.commit();
+				for(Appointment appointment : results){
+					appointment.setStatus("Cancelled");
+					noOfRecords = Integer.parseInt(appoinmentRepo.save(appointment).toString());
+				}
 
-			} catch (Exception hibernateException) { transaction.rollback(); log.error(hibernateException.getMessage(), hibernateException);
-				
+			} catch (Exception hibernateException) {
+				log.error(hibernateException.getMessage(), hibernateException);
 				hibernateException.printStackTrace();
+				throw hibernateException;
 
-			} finally {
-					HibernateUtil.closeSession();
-				return noOfRecords;
 			}
+				return noOfRecords;
+
 		}
 
         @Transactional
