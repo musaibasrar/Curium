3368eafa45c144bcc2b979866497476dbca03b2f
Mushy <musaibasrar@gmail.com>
2025-04-29 00:10:13 +0530

    1. removed financial year from the query 2. Swapped receipt and journal voucher type while fees stamping
    (cherry picked from commit 2efb70a62cd8c55b42851d24d455912f244b9ab9)

--- src/main/java/org/ideoholic/curium/model/feescollection/service/FeesCollectionService.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/service/FeesCollectionService.java
@@ -331,7 +331,7 @@ public class FeesCollectionService {
 			transactions.setCraccountid(crFees);
 			transactions.setDramount(onlyTotalFee);
 			transactions.setCramount(onlyTotalFee);
-			transactions.setVouchertype(1);
+			transactions.setVouchertype(4);
 			transactions.setTransactiondate(receiptInfo.getDate());
 			transactions.setEntrydate(DateUtil.todaysDate());
 			transactions.setNarration("Towards Fees Payment:  "+ackNoVoucherNarration+" "+chequeNoVoucherNarration);
@@ -357,7 +357,7 @@ public class FeesCollectionService {
 			transactionsIncome.setCraccountid(crFeesIncome);
 			transactionsIncome.setDramount(onlyTotalFee);
 			transactionsIncome.setCramount(onlyTotalFee);
-			transactionsIncome.setVouchertype(4);
+			transactionsIncome.setVouchertype(1);
 			transactionsIncome.setTransactiondate(receiptInfo.getDate());
 			transactionsIncome.setEntrydate(DateUtil.todaysDate());
 			transactionsIncome.setNarration("Towards Fees Payment:  "+ackNoVoucherNarration+" "+chequeNoVoucherNarration);
