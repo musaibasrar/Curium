286418b683b2f27a766d5a5305cf346f76351be3
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2025-04-29 07:24:20 +0530

    Migrate AccountDAO getVoucherEntryTransactions to use JPA repository

--- src/main/java/org/ideoholic/curium/model/account/dao/VoucherEntryTransactionsRepository.java
+++ src/main/java/org/ideoholic/curium/model/account/dao/VoucherEntryTransactionsRepository.java
@@ -1,5 +1,6 @@
 package org.ideoholic.curium.model.account.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.ideoholic.curium.model.account.dto.VoucherEntrytransactions;
@@ -20,8 +21,11 @@ public interface VoucherEntryTransactionsRepository extends JpaRepository<Vouche
 	    int cancelVoucher(@Param("id") int id);
 	    
 	    @Query("SELECT CASE WHEN count(v) > 0 THEN true ELSE false END FROM VoucherEntrytransactions v WHERE v.draccountid = :accountId OR v.craccountid = :accountId")
-	    boolean existsByDraccountidOrCraccountid(Integer accountId);
+	    boolean existsByDraccountidOrCraccountid(@Param("accountId") Integer accountId);
 	    
 	    List<VoucherEntrytransactions> findByNarrationLike(String supplierReferenceNo);
+	    
+	    @Query("SELECT v FROM VoucherEntrytransactions v WHERE v.transactiondate BETWEEN :fromDate AND :toDate AND v.financialyear = :financialYear AND v.cancelvoucher != 'yes' AND v.vouchertype = :voucherType AND v.branchid = :branchId ORDER BY v.transactionsid ASC")
+	    List<VoucherEntrytransactions> findVoucherEntries(@Param("fromDate") Date fromDate, @Param("toDate") Date toDate, @Param("financialYear") Integer financialYear, @Param("branchId") int branchId, @Param("voucherType") int voucherType);
 	 
 }
