c17c214212f592ddcb5e3d154ac4455daa4811be
Mushy <musaibasrar@gmail.com>
2025-05-02 20:11:05 +0530

    Search fee due for previous year
    (cherry picked from commit 601df1a0d4cb7bc9681abdb39206f6017871dd45)

--- src/main/java/org/ideoholic/curium/model/feescategory/dto/FeescategoryResponseDto.java
+++ src/main/java/org/ideoholic/curium/model/feescategory/dto/FeescategoryResponseDto.java
@@ -23,6 +23,8 @@ public class FeescategoryResponseDto {
 		
 	private List<Feescategory> feescategory;
 	private List<Parents> searchStudentList;
+	private String feesDueSearchYear;
+	private String feesDueSearchClass;
 	private boolean success;
 	
 	
