0bdd6e7a793bdc5ecc4e5653de48d942b64e5d46
Md Shahnawaz Ahmad <shahnawazahmad1050@gmail.com>
2025-05-03 16:37:38 +0530

    Migrated AppointmentDAO getNoOfRecordsTodayCompletedAppointments using SpringJPA

--- src/main/java/org/ideoholic/curium/model/appointment/dao/AppointmentDAO.java
+++ src/main/java/org/ideoholic/curium/model/appointment/dao/AppointmentDAO.java
@@ -215,31 +215,24 @@ public class AppointmentDAO {
 
 		}
 
-
+		@Transactional
 		public int getNoOfRecordsTodayCompletedAppointments() {
 			
-			List<Appointment> results = new ArrayList<Appointment>();
 			int noOfRecords = 0;
-			Transaction transaction = null;
 			try{
-				Session session = HibernateUtil.openCurrentSession();
-				transaction = session.beginTransaction();
 
-				results = (List<Appointment>) session.createQuery("From Appointment where status = 'Completed' and createddate = CURDATE()").setCacheable(true).setCacheRegion("commonregion")
-						.list();
-				noOfRecords = results.size();
-				log.info("The size of list is:::::::::::::::::::::::::::::::::::::::::: "
-								+ noOfRecords);
-				transaction.commit();
+				Long result = appointmentRepo.countByStatus("Completed");
+				noOfRecords = result.intValue();
 
-			} catch (Exception hibernateException) { transaction.rollback(); log.error(hibernateException.getMessage(), hibernateException);
-				
+			} catch (Exception hibernateException) {
+
+				log.error(hibernateException.getMessage(), hibernateException);
 				hibernateException.printStackTrace();
+				throw hibernateException;
 
-			} finally {
-					HibernateUtil.closeSession();
-				return noOfRecords;
 			}
+				return noOfRecords;
+
 		}
 
 
