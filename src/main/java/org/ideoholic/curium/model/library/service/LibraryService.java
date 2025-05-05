e80100a5be6d7046e0cfcf512cb6235421fd0af9
Mushy <musaibasrar@gmail.com>
2025-05-05 11:30:47 +0530

    Library updated

--- src/main/java/org/ideoholic/curium/model/library/service/LibraryService.java
+++ src/main/java/org/ideoholic/curium/model/library/service/LibraryService.java
@@ -2,6 +2,7 @@ package org.ideoholic.curium.model.library.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,25 +12,24 @@ import org.ideoholic.curium.dto.ResultResponse;
 import org.ideoholic.curium.model.library.dao.LibraryDAO;
 import org.ideoholic.curium.model.library.dto.Book;
 import org.ideoholic.curium.model.library.dto.BookDto;
+import org.ideoholic.curium.model.library.dto.BookHistory;
 import org.ideoholic.curium.model.library.dto.BooksRequestDto;
 import org.ideoholic.curium.model.library.dto.BooksResponseDto;
 import org.ideoholic.curium.util.DateUtil;
 
 public class LibraryService {
 	private HttpServletRequest request;
-    private HttpServletResponse response;
-    private HttpSession httpSession;
-    private String BRANCHID = "branchid";
-    
-    private static final int BUFFER_SIZE = 4096;
+	private HttpServletResponse response;
+	private HttpSession httpSession;
+	private String BRANCHID = "branchid";
 
-    
+	private static final int BUFFER_SIZE = 4096;
+
 	public LibraryService(HttpServletRequest request, HttpServletResponse response) {
 		this.request = request;
 		this.response = response;
 		this.httpSession = request.getSession();
 	}
-
 
 	public ResultResponse addBook(BookDto bookDto, String branchId) {
 
@@ -40,8 +40,8 @@ public class LibraryService {
 			book.setAuthor(bookDto.getAuthor());
 			book.setPublisher(bookDto.getPublisher());
 			book.setIsbn(bookDto.getIsbn());
-			book.setStatus(bookDto.getStatus());
-			book.setBookHolder(bookDto.getBookHolder());
+			book.setAvailableQty(bookDto.getAvailableqty());
+			book.setIssuedQty(bookDto.getIssuedqty());
 			book.setShelf(bookDto.getShelf());
 			book.setBookname(bookDto.getBookname());
 			book = new LibraryDAO().create(book);
@@ -50,6 +50,18 @@ public class LibraryService {
 		return ResultResponse.builder().success(false).build();
 	}
 
+	public void viewBooks() {
+
+		if (httpSession.getAttribute(BRANCHID) != null) {
+			try {
+				List<Book> list = new LibraryDAO().readListOfBook();
+				httpSession.setAttribute("book", list);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 
 	public BooksResponseDto viewBooks(String branchId) {
 		BooksResponseDto result = BooksResponseDto.builder().build();
@@ -84,17 +96,21 @@ public class LibraryService {
 
 	public BooksResponseDto viewBooksAvailable(String branchId) {
 		BooksResponseDto result = BooksResponseDto.builder().build();
-
+		
 		if (branchId != null) {
 			try {
 				List<Book> list = new LibraryDAO().readListOfBook();
 				List<Book> availableList = new ArrayList<>();
 				List<Book> issuedList = new ArrayList<>();
 				for (Book book : list) {
-					String status = book.getStatus();
-					if ("Available".equalsIgnoreCase(status)) {
+					int availableqty = book.getAvailableQty();
+					int issuedqty = book.getIssuedQty();
+					
+					if (availableqty > issuedqty) {
 						availableList.add(book);
-					} else if ("Issued".equalsIgnoreCase(status)) {
+					}
+
+					if (issuedqty > 0) {
 						issuedList.add(book);
 					}
 				}
@@ -109,7 +125,6 @@ public class LibraryService {
 		return result;
 
 	}
-
 
 	public ResultResponse updateBook(BooksRequestDto booksListDto) {
 
@@ -146,7 +161,6 @@ public class LibraryService {
 		return result;
 	}
 
-
 	public ResultResponse bookReturnByStudent(BooksRequestDto booksListDto) {
 
 		String[] bids = booksListDto.getBookIds();
@@ -161,7 +175,6 @@ public class LibraryService {
 		}
 		return ResultResponse.builder().build();
 	}
-
 
 	public BooksResponseDto viewBookdetails(String bookId, String branchId) {
 		BooksResponseDto result = BooksResponseDto.builder().build();
@@ -190,7 +203,8 @@ public class LibraryService {
 			String author = bookDto.getAuthor();
 			String publisher = bookDto.getPublisher();
 			String isbn = bookDto.getIsbn();
-			String status = bookDto.getStatus();
+			int availableQty = bookDto.getAvailableqty();
+			int issueQty = bookDto.getIssuedqty();
 			String shelf = bookDto.getShelf();
 			String uid = bookDto.getStudentExternalId();
 			String date = DateUtil.dateFromatConversionSlash(bookDto.getTransactionDate());
@@ -203,8 +217,62 @@ public class LibraryService {
 		return result;
 	}
         
-       
-        
-	
-}
+	public boolean viewBookdetails() {
+		boolean result = false;
+		int bid = Integer.parseInt(request.getParameter("id"));
+		if (httpSession.getAttribute(BRANCHID) != null) {
+			try {
+				Book list = new LibraryDAO().readDetailsOfBook(bid);
+				httpSession.setAttribute("book", list);
+				result = true;
+			} catch (Exception e) {
+				e.printStackTrace();
+				result = false;
+			}
+		}
+		return result;
 
+	}
+
+	public void updateBookitems() {
+		Book book = new Book();
+		int bid = Integer.parseInt(request.getParameter("bid"));
+		String bookname = request.getParameter("bookname");
+		String subject = request.getParameter("subject");
+		String author = request.getParameter("author");
+		String publisher = request.getParameter("publisher");
+		String isbn = request.getParameter("isbn");
+		String status = request.getParameter("status");
+		String shelf = request.getParameter("shelf");
+		String uid = request.getParameter("studentexternalid");
+		// String dates=request.getParameter("transactiondate");
+		String date = DateUtil.dateFromatConversionSlash(request.getParameter("transactiondate"));
+
+		new LibraryDAO().updatebookdetail(bid, bookname, subject, author, publisher, isbn, shelf);
+	}
+
+	public void getBookHistory() {
+
+		if (httpSession.getAttribute(BRANCHID) != null) {
+			try {
+				List<BookHistory> list = new LibraryDAO().readListOfBookHistory();
+				httpSession.setAttribute("bookhistory", list);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void deleteBookHistory() {
+		String[] idbook = request.getParameterValues("id");
+		if (idbook != null) {
+			List<Integer> ids = new ArrayList();
+			for (String id : idbook) {
+				System.out.println("id" + id);
+				ids.add(Integer.valueOf(id));
+			}
+			new LibraryDAO().deleteBookHistoryRecord(ids);
+		}
+	}
+
+}
