975bca070f5d0c66d5ddd5ddd17146951e5551e7
Mushy <musaibasrar@gmail.com>
2025-05-12 09:31:58 +0530

    Initial Setup

--- src/main/java/org/ideoholic/curium/model/feescategory/service/FeesService.java
+++ src/main/java/org/ideoholic/curium/model/feescategory/service/FeesService.java
@@ -293,7 +293,7 @@ public class FeesService {
 	 * } new feesCategoryDAO().waiveOffFees(sfsId,feesCatId,studentId);
 	 * 
 	 * return
-	 * "/abc/StudentProcess/ViewFeesStructure&id="+studentId; }
+	 * "/scholargroup/StudentProcess/ViewFeesStructure&id="+studentId; }
 	 * 
 	 * return "error.jsp";
 	 * 
