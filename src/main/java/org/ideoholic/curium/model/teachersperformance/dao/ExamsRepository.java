9a61fb27cd0d1c5be992b23e5ab5080ca45df1a6
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2025-05-13 07:57:21 +0530

    Migrate TeacherPerformanceDAO getExamsList to use JPA repository
    * Migrate TeacherPerformanceDAO getExamsList to use JPA repository
    
    * updated
    
    * added transactional

---
+++ src/main/java/org/ideoholic/curium/model/teachersperformance/dao/ExamsRepository.java
@@ -0,0 +1,12 @@
+package org.ideoholic.curium.model.teachersperformance.dao;
+
+import java.util.List;
+
+import org.ideoholic.curium.model.examdetails.dto.Exams;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExamsRepository extends JpaRepository<Exams, Integer>{
+	
+	List<Exams> findByBranchid(int branchid);
+
+}
