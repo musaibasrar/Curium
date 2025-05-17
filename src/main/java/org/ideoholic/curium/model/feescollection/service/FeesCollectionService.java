f04a3c498531114ddd6f75e94da26a71f889446b
Mushy <musaibasrar@gmail.com>
2025-05-17 23:59:30 +0530

    Added cheque payments to bank collection

--- src/main/java/org/ideoholic/curium/model/feescollection/service/FeesCollectionService.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/service/FeesCollectionService.java
@@ -2112,7 +2112,7 @@ public class FeesCollectionService {
 				String cashOrBank = receiptinfo.getPaymenttype();
 				if(cashOrBank.contains("Cash")) {
 					feesByCash = feesByCash+receiptinfo.getTotalamount();
-				}else if(cashOrBank.contains("Bank")) {
+				}else if(cashOrBank.contains("Bank") || cashOrBank.contains("Cheque")) {
 					feesByBank = feesByBank+receiptinfo.getTotalamount();
 				}
 			}
@@ -2179,7 +2179,7 @@ public class FeesCollectionService {
 			String cashOrBank = receiptinfo.getPaymenttype();
 			if(cashOrBank.contains("Cash")) {
 				feesByCashOtherFees = feesByCash+receiptinfo.getTotalamount();
-			}else if(cashOrBank.contains("Bank")) {
+			}else if(cashOrBank.contains("Bank") || cashOrBank.contains("Cheque")) {
 				feesByBankOtherFees = feesByBank+receiptinfo.getTotalamount();
 			}
 		}
