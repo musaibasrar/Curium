3b7eedeae90478b5a1a58cec84a42bf77911f4d0
Mushy <musaibasrar@gmail.com>
2025-05-17 10:20:03 +0530

    get the list of previous year based on the current year

--- src/main/java/org/ideoholic/curium/model/user/dto/UserAuthenticationResponseDto.java
+++ src/main/java/org/ideoholic/curium/model/user/dto/UserAuthenticationResponseDto.java
@@ -1,5 +1,7 @@
 package org.ideoholic.curium.model.user.dto;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
@@ -21,6 +23,8 @@ public class UserAuthenticationResponseDto {
     private String superUserAuth;
     private Integer userLoginId;
     private String attendanceStatus;
+    private String subBranchName;
+    private List<String> previousAcademicYears;
     @Builder.Default
     private boolean success = false;
 }
