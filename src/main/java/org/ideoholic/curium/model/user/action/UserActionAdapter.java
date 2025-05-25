9fd3c78572c287d01355c79245f559c854cd0899
Mushy <musaibasrar@gmail.com>
2025-05-25 20:51:10 +0530

    Fetch previous year list for multi user

--- src/main/java/org/ideoholic/curium/model/user/action/UserActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/user/action/UserActionAdapter.java
@@ -173,8 +173,9 @@ public class UserActionAdapter {
         httpSession.setAttribute("typeOfUser", responseDto.getTypeOfUser());
         httpSession.setAttribute("userAuth", responseDto.getUserAuth());
         httpSession.setAttribute("superuserAuth", responseDto.getSuperUserAuth());
-        httpSession.setAttribute(Constants.USERID, responseDto.getUserLoginId());
-
+        httpSession.setAttribute("userloginid", responseDto.getUserLoginId());
+        httpSession.setAttribute("subbranchname",responseDto.getSubBranchName());
+        httpSession.setAttribute("previousAcademicYears", responseDto.getPreviousAcademicYears());
         return responseDto.isSuccess();
     }
 
