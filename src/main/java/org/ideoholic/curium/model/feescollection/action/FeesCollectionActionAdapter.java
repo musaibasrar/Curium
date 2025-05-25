42f8e3199cd1b7def1caa26c20ee72678575f19c
Mushy <musaibasrar@gmail.com>
2025-05-25 20:22:28 +0530

    Narration printing bug fix resolved
    (cherry picked from commit d480a49feec6f31fa595f9785500e3dce024292e)

--- src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionActionAdapter.java
@@ -145,6 +145,7 @@ public class FeesCollectionActionAdapter {
         request.setAttribute("duplicate", responseDto.getDuplicate());
         request.setAttribute("user", responseDto.getUserLogin());
         httpSession.setAttribute("grandTotal", responseDto.getGrandTotal());
+        request.setAttribute("narrationreceipt", responseDto.getNarration());
     }
 
     public void getotherStampFees() {
@@ -447,6 +448,7 @@ public class FeesCollectionActionAdapter {
         request.setAttribute("recieptdate", responseDto.getReceiptDate());
         request.setAttribute("recieptinfo", responseDto.getReceiptInfo());
         request.setAttribute("feescatmap", responseDto.getFeeCatMap());
+        request.setAttribute("narrationreceipt", responseDto.getNarration());
     }
 
     public Otherreceiptinfo addother() {
