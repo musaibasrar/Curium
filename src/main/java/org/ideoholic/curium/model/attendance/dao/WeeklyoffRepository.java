2e508419bea8ca4e363a1548dbe5ee055803a83c
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2025-05-26 11:08:41 +0530

    Migrated AttendanceDAO readListOfWeeklyOff using SpringJPA

--- src/main/java/org/ideoholic/curium/model/attendance/dao/WeeklyoffRepository.java
+++ src/main/java/org/ideoholic/curium/model/attendance/dao/WeeklyoffRepository.java
@@ -12,4 +12,5 @@ public interface WeeklyoffRepository extends JpaRepository<Weeklyoff, Integer> {
 	List<Weeklyoff> findByAcademicyearAndBranchid(String academicyear, int branchid);
 
 	List<Weeklyoff> findByAcademicyearAndBranchidAndWidIn(String academicYear, int branchid, List<Integer> weeklyOffIds);
-}
+
+	List<Weeklyoff> findByAcademicyearAndWidIn(String academicYear, List<Integer> weeklyOffIds);}
