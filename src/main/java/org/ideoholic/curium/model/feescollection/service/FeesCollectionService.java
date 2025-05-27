4793318c8e9f89d1575910f6d86c51a7fd408ebb
Mushy <musaibasrar@gmail.com>
2025-05-27 23:33:55 +0530

    1. Path Changes 2. Import students changes

--- src/main/java/org/ideoholic/curium/model/feescollection/service/FeesCollectionService.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/service/FeesCollectionService.java
@@ -2492,9 +2492,10 @@ public class FeesCollectionService {
 					        }
 
 					System.out.println("Values Inserted Successfully");
-					if( new parentsDetailsDAO().createMultiple(listParents)) {
+					result = ResultResponse.builder().success(true).build();
+					/*if( new parentsDetailsDAO().createMultiple(listParents)) {
 						result = ResultResponse.builder().success(true).build();
-					};		
+					};*/		
 		return result;
 	}
 	
