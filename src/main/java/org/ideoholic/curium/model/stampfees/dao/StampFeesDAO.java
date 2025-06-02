26c822931f69c6d2dc067f7e7a2546bc5b17d4cd
Mushy <musaibasrar@gmail.com>
2025-06-02 09:03:46 +0530

    Stamp fee current balance Bug fix
    Signed-off-by: Mushy <musaibasrar@gmail.com>
    (cherry picked from commit 72ad13fc3e3f45a7d5751d182c158dbe90e0208f)

--- src/main/java/org/ideoholic/curium/model/stampfees/dao/StampFeesDAO.java
+++ src/main/java/org/ideoholic/curium/model/stampfees/dao/StampFeesDAO.java
@@ -160,13 +160,7 @@ public class StampFeesDAO {
 				
 					session.save(studentfeesstructure);
 					
-					//accounts
 					
-					session.save(transactions);
-					Query queryAccounts = session.createQuery(updateDrAccount);
-					queryAccounts.executeUpdate();
-					Query queryqueryAccounts1 = session.createQuery(updateCrAccount);
-					queryqueryAccounts1.executeUpdate();
 				
 				/*
 				 	Query query = session.createQuery("from Studentfeesstructure as sfs where sfs.sid = '"+studentfeesstructure.getSid()+"' and sfs.Feescategory.idfeescategory = '"+studentfeesstructure.getFeescategory().getIdfeescategory()+"' and sfs.academicyear = '"+currentYear+"'");
@@ -194,7 +188,13 @@ public class StampFeesDAO {
 				}*/
 		}
 			
-		
+			//accounts
+			
+			session.save(transactions);
+			Query queryAccounts = session.createQuery(updateDrAccount);
+			queryAccounts.executeUpdate();
+			Query queryqueryAccounts1 = session.createQuery(updateCrAccount);
+			queryqueryAccounts1.executeUpdate();
 			
 
 			transaction.commit();
