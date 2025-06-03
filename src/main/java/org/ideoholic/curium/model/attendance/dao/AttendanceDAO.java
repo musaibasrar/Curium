29c5d941552c7b39aa1cbcbd3b3bfdb3351f14a7
Md Shahnawaz Ahmad <shahnawazahmad1050@gmail.com>
2025-06-03 21:14:51 +0530

    renamed method  AttendanceDAO addAttendanceMaster using SpringJPA

--- src/main/java/org/ideoholic/curium/model/attendance/dao/AttendanceDAO.java
+++ src/main/java/org/ideoholic/curium/model/attendance/dao/AttendanceDAO.java
@@ -216,7 +216,7 @@ public class AttendanceDAO {
 		List<Attendancemaster> studentAttendanceMaster = new ArrayList<Attendancemaster>();
 		try{
 
-			studentAttendanceMaster = attendanceMasterRepo.findByAttendeeIdBranchId(attendeeId,branchId);
+			studentAttendanceMaster = attendanceMasterRepo.findByAttendeeidBranchid(attendeeId,branchId);
 
 		}catch (Exception e) {
 			log.error(e.getMessage(), e);
--- src/main/java/org/ideoholic/curium/model/attendance/dao/AttendanceMasterRepository.java
+++ src/main/java/org/ideoholic/curium/model/attendance/dao/AttendanceMasterRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface AttendanceMasterRepository extends JpaRepository<Attendancemaster, Integer> {
 
-    List<Attendancemaster> findByAttendeeIdBranchId(String attendeeId, int branchid);
+    List<Attendancemaster> findByAttendeeidBranchid(String attendeeId, int branchid);
 }
