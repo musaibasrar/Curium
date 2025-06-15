784590a36519df50a87023fcf9db991e69d851dc
Mushy <musaibasrar@gmail.com>
2025-06-15 22:20:08 +0530

    Teacher's Lesson Plan Creation Bug Fix

--- src/main/java/org/ideoholic/curium/model/job/action/JobActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/job/action/JobActionAdapter.java
@@ -156,7 +156,7 @@ public class JobActionAdapter {
 		addQueryDto.setAssignto(request.getParameterValues("assignto"));
 		addQueryDto.setTask(request.getParameterValues("task"));
 		addQueryDto.setDescription(request.getParameterValues("description"));
-		addQueryDto.setExpecteddd(request.getParameterValues("expecteddeliverydatetask"));
+		addQueryDto.setExpecteddeliverydatetask(request.getParameterValues("expecteddeliverydatetask"));
 		ResultResponse response = jobService.addQuery(addQueryDto,httpSession.getAttribute(BRANCHID).toString(),httpSession.getAttribute(CURRENTACADEMICYEAR).toString(),httpSession.getAttribute(USERLOGINID).toString());
 		return response.isSuccess();
 	}
