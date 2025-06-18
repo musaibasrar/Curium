cc60b197a33c8ffd7494c3f186e1c1122e6c6335
Mushy <musaibasrar@gmail.com>
2025-06-18 08:59:04 +0530

    Added Lombok to eventdto
    (cherry picked from commit f1abae6a30ba8e51fecc156df2e0b29684a06f3c)

---
+++ src/main/java/org/ideoholic/curium/model/event/dto/EventDTO.java
@@ -0,0 +1,19 @@
+package org.ideoholic.curium.model.event.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class EventDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String location;
+    private String color;
+    private boolean allDay;
+    private String createdBy;
+    private String branchId;
+} 
