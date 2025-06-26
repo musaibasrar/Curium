11d3dde9c334fd760a78f2c0c0655c8925741c1b
Reehan <reehan.lalkhan@gmail.com>
2025-06-26 15:18:21 +0530

    abcd

--- src/main/java/org/ideoholic/curium/model/attendance/dao/StudentDailyAttendanceRepository.java
+++ src/main/java/org/ideoholic/curium/model/attendance/dao/StudentDailyAttendanceRepository.java
@@ -1,16 +1,13 @@
 package org.ideoholic.curium.model.attendance.dao;
 
-import org.ideoholic.curium.model.attendance.dto.Studentdailyattendance;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import java.time.LocalDate;
 import java.util.Optional;
+
+import org.ideoholic.curium.model.attendance.dto.Studentdailyattendance;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentDailyAttendanceRepository extends JpaRepository<Studentdailyattendance,Integer> {
 
     Optional<Studentdailyattendance> findByAttendeeidAndDateAndAcademicyear(String attendeeid, LocalDate date, String academicyear);
 
-
-    boolean updateAttendanceStatus(String attendancestatus, String attendanceid);
 }
