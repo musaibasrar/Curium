6cb1e875001c61b1046139064f6ac56d50935480
Tauqueer <mdtauqueeralam031@gmail.com>
2025-07-01 11:32:02 +0530

    Migrate JobDAO viewTaskDetails to use JPA repository

--- src/main/java/org/ideoholic/curium/model/task/dao/TaskRepository.java
+++ src/main/java/org/ideoholic/curium/model/task/dao/TaskRepository.java
@@ -1,8 +1,12 @@
 package org.ideoholic.curium.model.task.dao;
 
+import java.util.List;
+
 import org.ideoholic.curium.model.task.dto.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+	
+	 List<Task> findByJobquery_Id(int jobId);
 
 }
