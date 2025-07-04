1c0234d7b7c724a3252578d32380751749999d3d
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2025-07-04 16:10:17 +0530

    Migrate JobDAO getNoOfRecordsTask to use JPA repository

--- src/main/java/org/ideoholic/curium/model/task/dao/TaskRepository.java
+++ src/main/java/org/ideoholic/curium/model/task/dao/TaskRepository.java
@@ -6,11 +6,15 @@ import org.ideoholic.curium.model.task.dto.Task;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 	
 	 List<Task> findByJobquery_Id(int jobId);
 	 
 	 Page<Task> findByBranchidOrderByIdDesc(int branchid, Pageable pageable);
+	 
+	 int countByBranchid(int branchId);
 
 }
