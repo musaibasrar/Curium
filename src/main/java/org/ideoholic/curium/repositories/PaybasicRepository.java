ed44112c5ff62559ffb4ec6c0bc568024ed955f5
Reehan <reehan.lalkhan@gmail.com>
2025-07-05 10:25:45 +0530

    Migrated EmployeeDAO readListOfEmployeesBasicPay to use JPA repository

---
+++ src/main/java/org/ideoholic/curium/repositories/PaybasicRepository.java
@@ -0,0 +1,8 @@
+package org.ideoholic.curium.repositories;
+
+import org.ideoholic.curium.model.hr.dto.Paybasic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaybasicRepository extends JpaRepository<Paybasic, Integer> {
+
+}
