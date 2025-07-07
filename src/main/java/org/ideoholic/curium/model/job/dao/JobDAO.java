3e46ab296c94f8be77fba136e2c7a0321c5256df
Tauqueer <mdtauqueeralam031@gmail.com>
2025-07-07 09:57:45 +0530

    Comments Added

--- src/main/java/org/ideoholic/curium/model/job/dao/JobDAO.java
+++ src/main/java/org/ideoholic/curium/model/job/dao/JobDAO.java
@@ -550,14 +550,22 @@ public class JobDAO {
 			
 			List<Task> result = new ArrayList<Task>();
 			try {
-				transaction = session.beginTransaction();
 				 jobQueryRepository.updateJobStatus(jobStatus,Date.from(Instant.now()), userId, jobId);
 				 for (Integer taskId : taskIdsList) {
 			            taskRepository.updateTaskToCompleted("Completed",Date.from(Instant.now()),userId, taskId);
 			        }
 				 result = taskRepository.findByIdIn(taskIdsList);
-				
-				transaction.commit();
+				 /*Query query = session.createQuery("update JobQuery set status = '"+jobStatus+"', updateddate = CURDATE(), updateduserid= "+userId+" where id="+jobId+"");
+				query.executeUpdate();
+
+				for (Integer appId : taskIdsList) {
+					Query task = session.createQuery("update Task set status = 'Completed', updateddate = CURDATE(), updateduserid= "+userId+" where id="+appId+"");
+					task.executeUpdate();
+					Task pq = new Task();
+					Query queryGet = session.createQuery("From Task as query where query.id = "+appId+"");
+					pq = (Task) queryGet.uniqueResult();
+					result.add(pq);
+				}*/
 			}  catch (Exception hibernateException) {
 				log.error(hibernateException.getMessage(), hibernateException);
 				hibernateException.printStackTrace();
