4c5a0cf43eaf25a8fe65175047f0dcf66a736498
Mushy <musaibasrar@gmail.com>
2025-07-11 20:38:07 +0530

    Archive student view per branch
    (cherry picked from commit 89c7a90658268989f47a1506a026b07fac1b462c)

--- src/main/java/org/ideoholic/curium/model/student/action/StudentActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/student/action/StudentActionAdapter.java
@@ -141,7 +141,7 @@ public class StudentActionAdapter {
 
     public void viewAllStudentsArchive() {
 
-        StudentAttendanceDetailsResponseDto responseDto = studentService.viewAllStudentsArchive();
+        StudentAttendanceDetailsResponseDto responseDto = studentService.viewAllStudentsArchive(httpSession.getAttribute(BRANCHID).toString());
         request.setAttribute("studentListArchive", responseDto.getStudentList());
     }
 
