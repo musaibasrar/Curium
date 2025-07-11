2c1b01edf60a868fce0d65618d8ab58c09c23299
Mushy <musaibasrar@gmail.com>
2025-07-11 20:37:23 +0530

    Mode of payment filter added to other fees collection
    (cherry picked from commit 1c5393d2e65796b22a205ea44e628339302eefb4)

--- src/main/java/org/ideoholic/curium/model/feescollection/dto/CancelledReceiptsDto.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/dto/CancelledReceiptsDto.java
@@ -12,4 +12,5 @@ public class CancelledReceiptsDto {
     private String toDateOtherFees;
     private String fromDateOtherFees;
     private String oneDayOtherFees;
+    private String modeOfPayment;
 }
