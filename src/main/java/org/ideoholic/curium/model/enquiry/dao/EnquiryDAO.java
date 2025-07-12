5db4c4dfc8cd14df4bbffae710972ea45b0a423f
Reehan <reehan.lalkhan@gmail.com>
2025-07-12 11:31:14 +0530

    Migrating enquiryDAO to EnquiryDAO to appropriate formatting

--- src/main/java/org/ideoholic/curium/model/enquiry/dao/EnquiryDAO.java
+++ src/main/java/org/ideoholic/curium/model/enquiry/dao/EnquiryDAO.java
@@ -11,21 +11,21 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class enquiryDAO {
-	
+public class EnquiryDAO {
+
 	@Autowired
 	private EnquiryRepository enquiryRepo;
-	
+
 	@Transactional
-	public void create(Enquiry enquiry ) {
+	public void create(Enquiry enquiry) {
 		try {
 			enquiryRepo.save(enquiry);
-        }catch (Exception hibernateException) { 
-        	log.error(hibernateException.getMessage(), hibernateException);
-            hibernateException.printStackTrace();
-            throw hibernateException;
+		} catch (Exception hibernateException) {
+			log.error(hibernateException.getMessage(), hibernateException);
+			hibernateException.printStackTrace();
 
-        } 
+			throw hibernateException;
+		}
 	}
 
 }
