c48221aa1745c34cb56b174c68ae5ca50abbd2c7
Tauqueer <mdtauqueeralam031@gmail.com>
2025-07-17 09:57:23 +0530

    Migrate feesDetailsDAO readList to use JPA repository

--- src/main/java/org/ideoholic/curium/repositories/FeesdetailsRepository.java
+++ src/main/java/org/ideoholic/curium/repositories/FeesdetailsRepository.java
@@ -1,8 +1,12 @@
 package org.ideoholic.curium.repositories;
 
+import java.util.List;
+
 import org.ideoholic.curium.model.feesdetails.dto.Feesdetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FeesdetailsRepository extends JpaRepository<Feesdetails, Integer> {
+	
+	List<Feesdetails> findBySidAndAcademicyear(int sid, String academicyear);
 
 }
