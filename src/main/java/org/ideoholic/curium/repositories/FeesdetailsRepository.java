74a590ceb1668a2c4c029a4246d9f5e8adc63ebc
Tauqueer <mdtauqueeralam031@gmail.com>
2025-07-18 16:32:39 +0530

    Migrate feesDetailsDAO feesSum to use JPA repository

--- src/main/java/org/ideoholic/curium/repositories/FeesdetailsRepository.java
+++ src/main/java/org/ideoholic/curium/repositories/FeesdetailsRepository.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 import org.ideoholic.curium.model.feesdetails.dto.Feesdetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FeesdetailsRepository extends JpaRepository<Feesdetails, Integer> {
 	
 	List<Feesdetails> findBySidAndAcademicyear(int sid, String academicyear);
+	
+    @Query("SELECT SUM(f.grandtotal) FROM Feesdetails f WHERE f.sid = :sid AND f.academicyear = :currentYear")
+    String sumGrandTotalBySidAndAcademicyear(@Param("sid") int sid, @Param("currentYear") String currentYear);
+
 
 }
