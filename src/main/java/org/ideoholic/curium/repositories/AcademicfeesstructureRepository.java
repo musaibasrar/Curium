accd17a6dab7c78454111d067a7b4abaf5e5d053
Tauqueer <mdtauqueeralam031@gmail.com>
2025-07-25 08:48:21 +0530

    Migrate feesDetailsDAO dueAmount to use JPA repository

---
+++ src/main/java/org/ideoholic/curium/repositories/AcademicfeesstructureRepository.java
@@ -0,0 +1,13 @@
+package org.ideoholic.curium.repositories;
+
+import org.ideoholic.curium.model.stampfees.dto.Academicfeesstructure;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface AcademicfeesstructureRepository extends JpaRepository<Academicfeesstructure, Integer> {
+	
+	@Query("SELECT a.totalfees FROM Academicfeesstructure a WHERE a.sid = :sid AND a.academicyear = :currentYear")
+    String getTotalFees(@Param("sid") int sid, @Param("currentYear") String currentYear);
+
+}
