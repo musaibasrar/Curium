e0c5aac8ae6ef299475aad39d1c1a1a71df7674e
Mushy <musaibasrar@gmail.com>
2025-07-27 22:06:08 +0530

    Father Name added in fees collection and student details fees structure
    (cherry picked from commit 7d4c3a19c04196091ffe6a5dbbefd54eef2e2628)

--- src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionActionAdapter.java
@@ -99,6 +99,7 @@ public class FeesCollectionActionAdapter {
         dto.setClassAndSec(request.getParameter("classandsec"));
         dto.setStudentId(request.getParameter("studentId"));
         dto.setDateOfFees(request.getParameter("dateoffees"));
+        dto.setFatherName(request.getParameter("fathername"));
 
         StampFeeResponseDto responseDto = feesCollectionService.getStampFees(dto, httpSession.getAttribute(CURRENTACADEMICYEAR).toString());
         request.setAttribute("studentfeesdetailspreviousyear", responseDto.getPreviousYearFeesMap());
@@ -109,6 +110,7 @@ public class FeesCollectionActionAdapter {
         request.setAttribute("classandsecDetails", responseDto.getClassAndSecDetails());
         request.setAttribute("studentIdDetails", responseDto.getStudentIdDetails());
         request.setAttribute("dateoffeesDetails", responseDto.getDateOfFeesDetails());
+        request.setAttribute("fatherNameDetails", responseDto.getFatherName());
     }
 
     public void getFeesDetails() {
