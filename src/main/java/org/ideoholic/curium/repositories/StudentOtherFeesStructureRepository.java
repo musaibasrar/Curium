33d3744efdc9f439cbd6dab125d177183d07e782
Reehan <reehan.lalkhan@gmail.com>
2025-07-28 21:02:32 +0530

    Migrating StudentDetailsDAO getStudentOtherFeesStructureDetails to use JPA repository

---
+++ src/main/java/org/ideoholic/curium/repositories/StudentOtherFeesStructureRepository.java
@@ -0,0 +1,8 @@
+package org.ideoholic.curium.repositories;
+
+import org.ideoholic.curium.model.student.dto.Studentotherfeesstructure;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentOtherFeesStructureRepository extends JpaRepository<Studentotherfeesstructure, Integer> {
+
+}
