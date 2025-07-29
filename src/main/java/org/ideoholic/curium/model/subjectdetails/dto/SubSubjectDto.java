3764ab415e475cd7ad65485f3fe25f71a193dd5d
Mushy <musaibasrar@gmail.com>
2025-07-29 11:40:09 +0530

    Sub Subject Marks Entry Module Added
    (cherry picked from commit 48815957346f9c6ac08fecb16d59aadc2b500693)

---
+++ src/main/java/org/ideoholic/curium/model/subjectdetails/dto/SubSubjectDto.java
@@ -0,0 +1,10 @@
+package org.ideoholic.curium.model.subjectdetails.dto;
+
+import lombok.Data;
+
+@Data
+public class SubSubjectDto {
+    private String subjectName;
+    private int subjectId;
+    private String[] subSubjects;
+}
