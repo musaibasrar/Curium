76d7325904e0fbbd786d5c2ee04278e3c9f48cbe
Tauqueer <mdtauqueeralam031@gmail.com>
2025-07-30 12:44:43 +0530

    Migrate feesDetailsDAO feesTotal to use JPA repository

--- src/main/java/org/ideoholic/curium/model/feesdetails/dao/feesDetailsDAO.java
+++ src/main/java/org/ideoholic/curium/model/feesdetails/dao/feesDetailsDAO.java
@@ -186,30 +186,22 @@ public class feesDetailsDAO {
                 }
                 return results;
         }
-
+      
+        @Transactional
         public String feesTotal(long id, String currentYear) {
-        	Session session = HibernateUtil.openCurrentSession(); 
-        	Transaction transaction = null;
-                 
+        	int sid = (int)id;
                 String results = "";
                 try {
-                    //this.session = HibernateUtil.getSessionFactory().openCurrentSession();
-
-                    transaction = session.beginTransaction();
                    
+                	// Query queryTotalFees =  session.createQuery("select totalfees From Academicfeesstructure as afs where afs.sid=" + id +"and afs.academicyear='"+currentYear+"'");
+                    results =  academicfeesstructureRepo.getTotalFees(sid, currentYear);          
+                               
                                 
-                                Query queryTotalFees =  session.createQuery("select totalfees From Academicfeesstructure as afs where afs.sid=" + id +"and afs.academicyear='"+currentYear+"'");
-                                results =  (String) queryTotalFees.uniqueResult();
-                                
-                  
-                    transaction.commit();
                 } catch (Exception hibernateException) { 
                 	log.error(hibernateException.getMessage(), hibernateException);
                     hibernateException.printStackTrace();
                     throw hibernateException;
-                }finally {
-        			HibernateUtil.closeSession();
-        		}
+                }
                 return results;
         }
 
