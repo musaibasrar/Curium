4ca50d3a87c7d3c89d0cbb9d5b9b74cade5d8e9e
Reehan <reehan.lalkhan@gmail.com>
2025-07-30 20:01:10 +0530

    Migrating studentDetailsDAO getCardDetails to use JPA repository

--- src/main/java/org/ideoholic/curium/repositories/CardRepository.java
+++ src/main/java/org/ideoholic/curium/repositories/CardRepository.java
@@ -1,8 +1,11 @@
 package org.ideoholic.curium.repositories;
 
+import java.util.List;
+
 import org.ideoholic.curium.model.mess.card.dto.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardRepository extends JpaRepository<Card, Integer> {
 
+	List<Card> findBySidIn(List<Integer> sids);
 }
