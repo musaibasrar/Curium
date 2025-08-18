4455411eecd489bc0b0827af353d9281ae10a595
Mushy <musaibasrar@gmail.com>
2025-08-18 23:00:24 +0530

    Fees outstanding report added which calculates all the previous year dues

--- src/main/java/org/ideoholic/curium/model/feescategory/action/FeesAction.java
+++ src/main/java/org/ideoholic/curium/model/feescategory/action/FeesAction.java
@@ -272,4 +272,11 @@ public class FeesAction {
 				e.printStackTrace();
 			}
 	}
+	
+	@GetMapping("/feesOutstanding")
+	public String feesOutstanding() {
+		standardActionAdapter.viewClasses();
+		feesActionAdapter.viewFees();
+		return "feesoutstanding";
+	}
 }
