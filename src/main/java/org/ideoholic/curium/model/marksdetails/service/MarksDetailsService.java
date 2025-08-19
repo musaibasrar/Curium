465543055c9d5597f1f47418cfd977bf62186557
Mushy <musaibasrar@gmail.com>
2025-08-19 17:19:16 +0530

    Marks Search by branch bug fix

--- src/main/java/org/ideoholic/curium/model/marksdetails/service/MarksDetailsService.java
+++ src/main/java/org/ideoholic/curium/model/marksdetails/service/MarksDetailsService.java
@@ -207,8 +207,8 @@ public class MarksDetailsService {
 		}
 
 		if (!classStudying.equalsIgnoreCase("")) {
-			querySub = " parents.student.classstudying like '" + classStudying
-					+ "' AND parents.student.archive=0 and parents.student.passedout=0 AND parents.student.droppedout=0 and parents.student.leftout=0";
+			querySub = " parents.Student.classstudying like '" + classStudying
+					+ "' AND parents.Student.archive=0 and parents.Student.passedout=0 AND parents.Student.droppedout=0 and parents.Student.leftout=0 AND parents.branchid="+Integer.parseInt(branchId);
 		} else if (classStudying.equalsIgnoreCase("") && !querySub.equalsIgnoreCase("")) {
 			querySub = querySub + " AND parents.student.archive=0 and parents.student.passedout=0 AND parents.student.droppedout=0 and parents.student.leftout=0 AND parents.branchid="+Integer.parseInt(branchId);
 		}
@@ -1830,8 +1830,8 @@ public SearchStudentResponseDto SearchForTeacher(EmployeeDetailsResponseDto empl
 	}
 
 	if (!classStudying.equalsIgnoreCase("")) {
-		querySub = " parents.student.classstudying like '" + classStudying
-				+ "' AND parents.student.archive=0 and parents.student.passedout=0 AND parents.student.droppedout=0 and parents.student.leftout=0";
+		querySub = " parents.Student.classstudying like '" + classStudying
+				+ "' AND parents.Student.archive=0 and parents.Student.passedout=0 AND parents.Student.droppedout=0 and parents.Student.leftout=0 AND parents.branchid="+Integer.parseInt(branchId);
 	} else if (classStudying.equalsIgnoreCase("") && !querySub.equalsIgnoreCase("")) {
 		querySub = querySub + " AND parents.student.archive=0 and parents.student.passedout=0 AND parents.student.droppedout=0 and parents.student.leftout=0 AND parents.branchid="+Integer.parseInt(branchId);
 	}
