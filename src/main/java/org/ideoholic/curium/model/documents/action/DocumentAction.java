2aa3abfb43b4a73b4c786f39daeea3f4d6210d70
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2025-08-26 11:22:47 +0530

    Tc Report print
    * vision tc report print
    
    * vision tc report print updated
    
    (cherry picked from commit 393734f2dcbf7d559cd115d722261d253771d5c8)

--- src/main/java/org/ideoholic/curium/model/documents/action/DocumentAction.java
+++ src/main/java/org/ideoholic/curium/model/documents/action/DocumentAction.java
@@ -210,4 +210,26 @@ public class DocumentAction {
 		documentActionAdapter.printCharacterCertificate();
 		return "characterprint";
 	}
+	
+	@GetMapping("/getTcDetail")
+	public String getTcDetail() {
+		return "generatetcdetail";
+	}
+	
+	@GetMapping("/tcDetail")
+	public String tcDetail() {
+		documentActionAdapter.viewTcDetail(); 
+		return "studentstcreport";
+	}
+	
+	@PostMapping("/printTcList")
+	public String printTcList() {
+		
+		if(documentActionAdapter.printTcList()){
+			return "printtclist";
+		}else{
+			return "error";
+		}
+		
+	}
 }
