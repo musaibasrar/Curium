4680245001be4dfea7075c025a416632f7394792
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2025-08-26 11:22:47 +0530

    Tc Report print
    * vision tc report print
    
    * vision tc report print updated
    
    (cherry picked from commit 393734f2dcbf7d559cd115d722261d253771d5c8)

--- src/main/java/org/ideoholic/curium/model/documents/action/DocumentActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/documents/action/DocumentActionAdapter.java
@@ -197,5 +197,20 @@ public class DocumentActionAdapter {
 		ResultResponse resultResponse=documentService.downlaodFile();
 		return resultResponse.isSuccess();
 	}
+	
+	public void viewTcDetail() {
+
+		CharacterResponseDto characterResponseDto = documentService.viewTcDetail();
+		request.setAttribute("studenttcissued", characterResponseDto.getListofParents());
+		
+	}
+
+	public boolean printTcList() {
+		CharacterDto characterDto = new CharacterDto();
+		characterDto.setFeesIds(request.getParameterValues("studentIDs"));
+		CharacterResponseDto characterResponseDto = documentService.printTcList(characterDto);
+		request.setAttribute("studenttcissued", characterResponseDto.getListofParents());
+		return characterResponseDto.isSuccess();
+	}
 
 }
