35f4f8987e5a1972c28e48f619ed9aa07d3d07d1
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2025-10-06 13:11:01 +0530

    attendance added in report card
    * attendance added in report card
    
    * Report Card Changes
    
    * space removed

--- src/main/java/org/ideoholic/curium/model/attendance/dao/AttendanceDAO.java
+++ src/main/java/org/ideoholic/curium/model/attendance/dao/AttendanceDAO.java
@@ -3,6 +3,7 @@ package org.ideoholic.curium.model.attendance.dao;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -636,19 +637,49 @@ List<Staffdailyattendance> staffDailyAttendance = new ArrayList<Staffdailyattend
 		return attendance;
 	}
 	
-	public List<Studentdailyattendance> getStudentTotalAttendance(
-			String studentExternalId,
-			String currentAcademicYear, int branchId) {
+	/*
+	 * public List<Studentdailyattendance> getStudentTotalAttendance( String
+	 * studentExternalId, String currentAcademicYear, int branchId) {
+	 * List<Studentdailyattendance> studentDailyAttendance = new
+	 * ArrayList<Studentdailyattendance>(); try { transaction =
+	 * session.beginTransaction(); studentDailyAttendance =
+	 * session.createQuery("from Studentdailyattendance  where  academicyear = '"
+	 * +currentAcademicYear+"' and attendeeid = '"
+	 * +studentExternalId+"' and branchid="+branchId).list(); transaction.commit();
+	 * } catch (Exception e) { transaction.rollback(); logger.error(e); // TODO:
+	 * handle exception }finally { HibernateUtil.closeSession(); } return
+	 * studentDailyAttendance; }
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Studentdailyattendance> getStudentTotalAttendanceDateWise(String studentexternalid,
+			String currentAcademicYear, int branchid, Date datePresent) {
 		List<Studentdailyattendance> studentDailyAttendance = new ArrayList<Studentdailyattendance>();
-		try {
-			transaction = session.beginTransaction();
-			studentDailyAttendance = session.createQuery("from Studentdailyattendance  where  academicyear = '"+currentAcademicYear+"' and attendeeid = '"+studentExternalId+"' and branchid="+branchId).list();
-			transaction.commit();
-		} catch (Exception e) { transaction.rollback(); logger.error(e);
-			// TODO: handle exception
-		}finally {
-			HibernateUtil.closeSession();
-		}
+		    Transaction transaction = null;
+		    Session session = HibernateUtil.openCurrentSession();
+
+		    try {
+		        transaction = session.beginTransaction();
+
+		        // Query: from start (no lower bound) till given date
+		        studentDailyAttendance = session.createQuery(
+		                "from Studentdailyattendance " +
+		                "where date <= :datePresent " +
+		                "and academicyear = :currentAcademicYear " +
+		                "and attendeeid = :studentexternalid " +
+		                "and branchid = :branchid")
+		                .setParameter("datePresent", datePresent)
+		                .setParameter("currentAcademicYear", currentAcademicYear)
+		                .setParameter("studentexternalid", studentexternalid)
+		                .setParameter("branchid", branchid)
+		                .list();
+
+		        transaction.commit();
+		    } catch (Exception e) {
+		        if (transaction != null) transaction.rollback();
+		        logger.error("Error while fetching student attendance", e);
+		    } finally {
+		        HibernateUtil.closeSession();
+		    }
 		return studentDailyAttendance;
 	}
 
