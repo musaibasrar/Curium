78965d3d44cfcb924eb4cd7a511ed22eb26d5beb
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2025-10-21 11:20:45 +0530

    Attendance Added in marksheet
    * Attendance Added in marksheet
    
    * updated attendance added

--- src/main/java/org/ideoholic/curium/model/marksdetails/service/MarksDetailsService.java
+++ src/main/java/org/ideoholic/curium/model/marksdetails/service/MarksDetailsService.java
@@ -573,7 +573,7 @@ public class MarksDetailsService {
 			String[] studentIds = dto.getStudentIds();
 			String examC = dto.getExamClass();
 			String[] examClass = examC.split("--");
-			String presentDate = dto.getNoofpresentday();
+			String presentDate = dto.getTotalDaysPresent();
 			//String totalColumnNumber = new DataUtil().getPropertiesValue("totalColumnNumber");
 			//String[][] marksList = new String[studentIds.length][Integer.parseInt(totalColumnNumber)+1];
 			List<Exams> examsList = new ExamDetailsDAO().readListOfExams(Integer.parseInt(branchId));
@@ -1656,6 +1656,7 @@ public GenerateReportResponseDto generateReportSingleExams(GenerateReportDto dto
 		String examC = dto.getExamClass();
 		String[] examClass = examC.split("--");
 		List<Integer> examIds = new ArrayList<Integer>();
+		String presentDate = dto.getTotalDaysPresent();
 		//String totalColumnNumber = new DataUtil().getPropertiesValue("totalColumnNumber");
 		//String[][] marksList = new String[studentIds.length][Integer.parseInt(totalColumnNumber)+1];
 		for (String examId : dto.getExamIds()) {
@@ -1670,6 +1671,31 @@ public GenerateReportResponseDto generateReportSingleExams(GenerateReportDto dto
 			ExamRank examrank = new ExamRank();
 			List<ExamsMarks> examMarksList = new ArrayList<ExamsMarks>();
 			Parents studentDetails = new studentDetailsDAO().readUniqueObjectParents(Integer.parseInt(studentIds[i]));
+			
+			List<Studentdailyattendance> studentDailyAttendance = new ArrayList<Studentdailyattendance>();
+			studentDailyAttendance = new AttendanceDAO().getStudentTotalAttendanceDateWise(studentDetails.getStudent().getStudentexternalid(), currentAcademicYear,Integer.parseInt(branchId),
+					DateUtil.indiandateParser(presentDate));
+			int absentDays = 0;
+			int totalDays = 0;
+			int totalPresent = 0;
+			
+			for (Studentdailyattendance dailyattendance : studentDailyAttendance) {
+				
+				totalDays++;
+				if(("A").equalsIgnoreCase(dailyattendance.getAttendancestatus())){
+					absentDays++;
+				}
+				
+			}
+			
+			if(!studentDailyAttendance.isEmpty()){
+				totalPresent = totalDays - absentDays;
+			}
+			
+			result.setTotalDays(totalDays);
+			result.setTotalpresent(totalPresent);
+			result.setTotalabsent(absentDays);
+			
 			markssheet.setParents(studentDetails);
 			
 			for (Exams exam : examsList) {
