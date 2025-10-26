aa7fe94164d7677f9c5317bc2378e2c2dbd7aee8
Mushy <musaibasrar@gmail.com>
2025-10-26 11:20:57 +0530

    Admission Abstract

--- src/main/java/org/ideoholic/curium/model/documents/action/DocumentAction.java
+++ src/main/java/org/ideoholic/curium/model/documents/action/DocumentAction.java
@@ -303,5 +303,12 @@ public class DocumentAction {
 		public String printArticleCertificate() {
 			return "articlecertificateprint";
 		}
+	  
+	  @PostMapping("/printAdmissionAbstract")
+		public String printAdmissionAbstract() {
+
+			documentActionAdapter.printAdmissionAbstract();
+			return "printpreviewabstract";
+		}
 	 
 }
