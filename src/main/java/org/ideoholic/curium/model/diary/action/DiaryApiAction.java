04fa70c1b995f7c27a27e20459dbbedce8f866a6
Mushy <musaibasrar@gmail.com>
2025-11-15 10:16:13 +0530

    removed branchid from viewDiaryStudentParent
    (cherry picked from commit 61d3e6616b180760693382f7470f5d5d82e84eb5)

--- src/main/java/org/ideoholic/curium/model/diary/action/DiaryApiAction.java
+++ src/main/java/org/ideoholic/curium/model/diary/action/DiaryApiAction.java
@@ -3,41 +3,39 @@ package org.ideoholic.curium.model.diary.action;
 import org.ideoholic.curium.dto.ResultResponse;
 import org.ideoholic.curium.model.diary.dto.AddDiaryDto;
 import org.ideoholic.curium.model.diary.dto.DairyIdsDto;
-import org.ideoholic.curium.model.diary.dto.DiaryDetailsMessageResponseDto;
 import org.ideoholic.curium.model.diary.dto.DiaryResponseDto;
+import org.ideoholic.curium.model.diary.dto.DiaryDetailsMessageResponseDto;
 import org.ideoholic.curium.model.student.dto.StudentIdDto;
 import org.ideoholic.curium.model.student.dto.StudentIdPageDto;
-import org.ideoholic.curium.util.Constants;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface DiaryApiAction {
-	
-	 ResponseEntity<ResultResponse> getdiarystudent(@RequestHeader(value = Constants.BRANCHID) String branchId) ;
+
+	 ResponseEntity<ResultResponse> getdiarystudent(@RequestHeader(value = "branchid") String branchId) ;
 
 	 ResponseEntity addDiary(@RequestBody AddDiaryDto addDiaryDto,
-			@RequestHeader(value = Constants.BRANCHID) String branchId,
-			@RequestHeader(value = Constants.CURRENTACADEMICYEAR) String currentAcademicYear,
-			@RequestHeader(value = Constants.USERID) String userLoginId);
+			@RequestHeader(value = "branchid") String branchId,
+			@RequestHeader(value = "currentAcademicYear") String currentAcademicYear,
+			@RequestHeader(value = "userloginid") String userLoginId);
 
-	 ResponseEntity<DiaryResponseDto> viewdiarystudent(@RequestHeader(value = Constants.BRANCHID) String branchId, @RequestParam(value="page")
+	 ResponseEntity<DiaryResponseDto> viewdiarystudent(@RequestHeader(value = "branchid") String branchId, @RequestParam(value="page")
 			String page) ;
 
-	 ResponseEntity<DiaryResponseDto> viewDiaryStudentParent(@RequestBody StudentIdPageDto studentIdPageDto,
-			@RequestHeader(value = Constants.BRANCHID) String branchId);
+	 ResponseEntity<DiaryResponseDto> viewDiaryStudentParent(@RequestBody StudentIdPageDto studentIdPageDto);
 
 	 ResponseEntity<DiaryResponseDto> deleteRecord(@RequestBody DairyIdsDto dairyIdsDto,
-			@RequestHeader(value = Constants.BRANCHID) String branchId,@RequestParam(value="page")
+			@RequestHeader(value = "branchid") String branchId,@RequestParam(value="page")
 		String page) ;
 
      ResponseEntity<String> diarySaved(); 
 		
 
 	 ResponseEntity<DiaryDetailsMessageResponseDto> viewDiaryDetails(
-			@RequestBody StudentIdDto studentIdDto);
+			@RequestBody StudentIdDto studentIdDto) ;
 
 	 ResponseEntity<DiaryDetailsMessageResponseDto> viewDiaryDetailsParent(
-			@RequestBody StudentIdDto studentIdDto);
+			@RequestBody StudentIdDto studentIdDto) ;
 }
