0951efd4d857e5cd347a1064780422a28917f1c8
Mushy <musaibasrar@gmail.com>
2025-11-23 09:26:14 +0530

    1. added method to get the months from util 2. added method to search fees category by name

--- src/main/java/org/ideoholic/curium/model/feescategory/action/FeesAction.java
+++ src/main/java/org/ideoholic/curium/model/feescategory/action/FeesAction.java
@@ -130,6 +130,7 @@ public class FeesAction {
 	public String viewFees() {
 		feesActionAdapter.viewFees();
 		standardActionAdapter.viewClasses();
+		feesActionAdapter.getFeesMonths();
 		return "feesCategory";
 	}
 
