45d1a932c52260e327369da19be68931b11f2c09
Mushy <musaibasrar@gmail.com>
2025-12-16 15:17:07 +0530

    Marks entry bug fix

--- src/main/java/org/ideoholic/curium/model/marksdetails/service/MarksDetailsService.java
+++ src/main/java/org/ideoholic/curium/model/marksdetails/service/MarksDetailsService.java
@@ -2006,7 +2006,7 @@ private List<Marks> prepareSubSubjectMarks(String subSubjectName, String[] stude
         Subject subjectDetails, int examId,
         String academicYear, String userId, String branchId) {
 		
-		SubSubject subSubject = new SubjectDetailsDAO().readSubSubject(Integer.parseInt(branchId), subjectDetails.getSubid(), subSubjectName);
+		SubSubject subSubject = new SubjectDetailsDAO().readSubSubject(Integer.parseInt(branchId), subjectDetails.getSubjectid(), subSubjectName);
 		if (subSubject == null) return Collections.emptyList();
 		
 		return buildMarksList(studentIds, marksArray, subjectDetails, examId, academicYear, userId, branchId, Collections.emptyList(), subSubject.getId());
