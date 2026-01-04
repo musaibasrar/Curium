08a30540b8e52ac5f471dfc64ee0dd75aacd7118
Mushy <musaibasrar@gmail.com>
2026-01-04 07:41:26 +0530

    Mark student as inactive when TC is issued

--- src/main/java/org/ideoholic/curium/model/documents/service/DocumentService.java
+++ src/main/java/org/ideoholic/curium/model/documents/service/DocumentService.java
@@ -115,12 +115,11 @@ public class DocumentService {
 		String datecert = DataUtil.emptyString(transferCertificateDto.getDateCert());
 		String Remarks = DataUtil.emptyString(transferCertificateDto.getRemarks());
 		Date dateOfTc = DateUtil.dateParserUpdateStd(transferCertificateDto.getDateOfTc());
+		String studentAdmissionStatus = DataUtil.emptyString(transferCertificateDto.getStudentAdmissionStatus());
 		
 		student.setReasonleaving(leavingReason);
 		student.setSid(studentId);
-		 boolean updateStudent = new studentDetailsDAO().updateStudent(student);
 		 
-		 if(updateStudent){
 			 tc.setSid(studentId);
 			 tc.setApplicationstatus("applied");
 			 tc.setDateofissues(dateOfTc);
@@ -161,9 +160,9 @@ public class DocumentService {
 				 transferCertificateResponseDto.setStatus(TransferStatus.TCEXISTS);
 				 return transferCertificateResponseDto;
 			 }else {
-					transferCertificateString = new DocumentDAO().generateTransferCertificate(tc);
+					transferCertificateString = new DocumentDAO().generateTransferCertificate(tc,student,studentAdmissionStatus);
 			}
-		 }
+		 
 		 
 		 if("true".equalsIgnoreCase(transferCertificateString)){
 			 String getStudentInfo  = "from Parents as parents where parents.Student.sid="+studentId;
