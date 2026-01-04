3b0bfba17c4adf8dda6e472c38a09c4499b2f236
Mushy <musaibasrar@gmail.com>
2026-01-04 07:41:26 +0530

    Mark student as inactive when TC is issued

--- src/main/java/org/ideoholic/curium/model/documents/dao/DocumentDAO.java
+++ src/main/java/org/ideoholic/curium/model/documents/dao/DocumentDAO.java
@@ -10,6 +10,7 @@ import org.hibernate.query.Query;
 import org.ideoholic.curium.model.documents.dto.StudyCertificate;
 import org.ideoholic.curium.model.documents.dto.Transfercertificate;
 import org.ideoholic.curium.model.parents.dto.Parents;
+import org.ideoholic.curium.model.student.dto.Student;
 import org.ideoholic.curium.util.HibernateUtil;
 import org.ideoholic.curium.util.Session;
 import org.ideoholic.curium.util.Session.Transaction;
@@ -34,11 +35,14 @@ public class DocumentDAO {
 	
 	
 
-	public String generateTransferCertificate(Transfercertificate tc) {
+	public String generateTransferCertificate(Transfercertificate tc, Student student, String studentAdmissionStatus) {
 		String status = "false";
 		try {
 			transaction = session.beginTransaction();
 			session.save(tc);
+			Query queryUpdate = session
+					.createQuery("update Student set reasonleaving = '"+student.getReasonleaving()+"',"+studentAdmissionStatus+"='1'  where sid = '"+student.getSid()+"'");
+			queryUpdate.executeUpdate();
 			transaction.commit();
 			status = "true";
 		} catch (Exception e) { 
