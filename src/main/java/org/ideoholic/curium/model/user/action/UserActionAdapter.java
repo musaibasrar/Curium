759a6c8263a36c4ddbd5cdbf661eacd5a7da8475
Mushy <musaibasrar@gmail.com>
2026-01-05 15:40:39 +0530

    added academic year filter in fees collection details report

--- src/main/java/org/ideoholic/curium/model/user/action/UserActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/user/action/UserActionAdapter.java
@@ -48,10 +48,12 @@ public class UserActionAdapter {
         dto.setFromDate(request.getParameter("fromdate"));
         dto.setOneDay(request.getParameter("oneday"));
         dto.setModeOfPayment(request.getParameter("modeofpayment"));
+        dto.setAcademicYear(request.getParameter("academicyear"));
         
         String dayOne = httpSession.getAttribute("dayone") == null ? null : httpSession.getAttribute("dayone").toString();
         String dayFrom = httpSession.getAttribute("datefrom") == null ? null : httpSession.getAttribute("dayone").toString();
         String dateTo = httpSession.getAttribute("dateto") == null ? null : httpSession.getAttribute("dayone").toString();
+        
 
         SearchByDateResponseDto responseDto = userService.searchByDate(dto, httpSession.getAttribute(Constants.BRANCHID).toString(), dayOne, dayFrom, dateTo);
         httpSession.setAttribute("feesdetailsbranchname", responseDto.getFeesDetailsBranchName());
