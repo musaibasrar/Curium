43bdbc324153afbfdfc0b2bb7954147c3a3bc239
Mushy <musaibasrar@gmail.com>
2026-01-06 22:34:47 +0530

    Added Fees Report for Inactive students

--- src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionActionAdapter.java
@@ -61,7 +61,7 @@ public class FeesCollectionActionAdapter {
         dto.setAcademicYear(request.getParameter("academicyear"));
         dto.setFeesCat(request.getParameterValues("feescategory"));
         dto.setAddClass(request.getParameterValues("classsearch"));
-
+        dto.setStudentType(request.getParameter("studenttype"));
         ResultResponse resultResponse = feesCollectionService.getFeesReport(dto, httpSession.getAttribute(Constants.BRANCHID).toString(), httpSession.getAttribute(Constants.CURRENTACADEMICYEAR).toString());
         httpSession.setAttribute("studentfeesreportlist", resultResponse.getResultList());
     }
