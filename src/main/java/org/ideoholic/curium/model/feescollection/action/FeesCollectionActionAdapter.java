737146ffc382b480eeec46eeabf5157994cc6581
Mushy <musaibasrar@gmail.com>
2026-01-06 22:27:58 +0530

    Constants File Added

--- src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionActionAdapter.java
@@ -35,6 +35,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+
 @Service
 public class FeesCollectionActionAdapter {
 
