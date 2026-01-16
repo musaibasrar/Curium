ebd5df9e444ed89334f992004da51a3ebd6b5b1c
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2026-01-16 21:15:55 +0530

    print button added in feesreport and feeduereport

--- src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionActionAdapter.java
@@ -59,6 +59,9 @@ public class FeesCollectionActionAdapter {
         dto.setStudentType(request.getParameter("studenttype"));
         ResultResponse resultResponse = feesCollectionService.getFeesReport(dto, httpSession.getAttribute(BRANCHID).toString(), httpSession.getAttribute(CURRENTACADEMICYEAR).toString());
         httpSession.setAttribute("studentfeesreportlist", resultResponse.getResultList());
+        request.setAttribute("selectedClassList", dto.getAddClass());
+        request.setAttribute("selectedFeesCategoryList", dto.getFeesCat());
+
     }
 
     public void undoFeesReceipt() {
@@ -311,6 +314,8 @@ public class FeesCollectionActionAdapter {
 
         ResultResponse resultResponse = feesCollectionService.getFeesReportDue(dto, httpSession.getAttribute(BRANCHID).toString(), httpSession.getAttribute(CURRENTACADEMICYEAR).toString());
         httpSession.setAttribute("studentfeesreportlist", resultResponse.getResultList());
+        request.setAttribute("selectedFeesCategoryList", dto.getFeesCat());
+        request.setAttribute("selectedClassList", dto.getAddClass());
     }
 
     public void getFeesStampDueReport() {
