3fb23f3dcfc5112e85f09c546a231eb06a4e3484
Mushy <musaibasrar@gmail.com>
2026-01-23 19:46:25 +0530

    1. removed super user session variable 2. added message templates

--- src/main/java/org/ideoholic/curium/model/user/action/UserActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/user/action/UserActionAdapter.java
@@ -159,7 +159,7 @@ public class UserActionAdapter {
 
         String branchId = request.getParameter(BRANCHID);
 
-        UserAuthenticationResponseDto responseDto = userService.authenticateMultiUser(httpSession.getAttribute("username").toString(), httpSession.getAttribute("superuserAuth").toString(), branchId);
+        UserAuthenticationResponseDto responseDto = userService.authenticateMultiUser(httpSession.getAttribute("username").toString(), branchId);
         httpSession.setAttribute("currentAcademicYear", responseDto.getAcademicYear());
         httpSession.setAttribute("username", responseDto.getUserName());
         httpSession.setAttribute("branchid", responseDto.getBranchId());
