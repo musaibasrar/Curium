58766cc5b1ddbd9afbaed49e7c78cdb6aec27e9c
Mushy <musaibasrar@gmail.com>
2026-01-27 11:28:46 +0530

    added filter in stamp fee by academic year

--- src/main/java/org/ideoholic/curium/model/stampfees/service/StampFeesService.java
+++ src/main/java/org/ideoholic/curium/model/stampfees/service/StampFeesService.java
@@ -473,7 +473,8 @@ public class StampFeesService {
     		String conClassStudying = "";
     		String querySub = "";
     		String classStudying = "";
-
+    		String academicYear = searchStudentDto.getCategoryYear();
+    		
     		switch (studentType) {
 			case "Active":
 				if (!addClass.equalsIgnoreCase("")) {
@@ -487,7 +488,7 @@ public class StampFeesService {
 	    		classStudying = DataUtil.emptyString(conClassStudying);
 
 	    		if (!studentname.equalsIgnoreCase("")) {
-	    			querySub = " parents.Student.name like '%" + studentname + "%' AND parents.Student.archive=0 and parents.Student.passedout=0 AND parents.Student.droppedout=0 and parents.Student.leftout=0 AND parents.Student.branchid="+Integer.parseInt(branchid);
+	    			querySub = " parents.Student.name like '%" + studentname + "%' AND parents.Student.archive=0 and parents.Student.passedout=0 AND parents.Student.droppedout=0 and parents.Student.leftout=0 AND (parents.Student.yearofadmission='"+academicYear+"' OR parents.Student.promotedyear='"+academicYear+"')  AND parents.Student.branchid="+Integer.parseInt(branchid);
 	    		}
 
 	    		if (!classStudying.equalsIgnoreCase("")
@@ -496,7 +497,7 @@ public class StampFeesService {
 	    					+ classStudying + "' AND parents.Student.archive=0 and parents.Student.passedout=0 AND parents.Student.droppedout=0 and parents.Student.leftout=0";
 	    		} else if (!classStudying.equalsIgnoreCase("")) {
 	    			querySub = querySub + " parents.Student.classstudying like '"
-	    					+ classStudying + "' AND parents.Student.archive=0 and parents.Student.passedout=0 AND parents.Student.droppedout=0 and parents.Student.leftout=0 AND parents.Student.branchid="+Integer.parseInt(branchid)+" order by parents.Student.admissionnumber ASC";
+	    					+ classStudying + "' AND parents.Student.archive=0 and parents.Student.passedout=0 AND parents.Student.droppedout=0 and parents.Student.leftout=0 AND (parents.Student.yearofadmission='"+academicYear+"' OR parents.Student.promotedyear='"+academicYear+"')  AND parents.Student.branchid="+Integer.parseInt(branchid)+" order by parents.Student.admissionnumber ASC";
 	    		}
 				break;
 			case "InActive":
