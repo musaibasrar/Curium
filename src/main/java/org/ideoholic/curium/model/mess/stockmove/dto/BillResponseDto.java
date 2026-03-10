b4b255994fc368b55d3c2c18c396b830479e8fca
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2026-03-10 16:16:40 +0530

    parameter and print button added in report
    (cherry picked from commit dd7f43e564929cd594bb3381d62a9371c1ed8bc4)

--- src/main/java/org/ideoholic/curium/model/mess/stockmove/dto/BillResponseDto.java
+++ src/main/java/org/ideoholic/curium/model/mess/stockmove/dto/BillResponseDto.java
@@ -1,5 +1,7 @@
 package org.ideoholic.curium.model.mess.stockmove.dto;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -16,5 +18,12 @@ public class BillResponseDto {
 	 private double sumSgst;
 	 private double sumCgst;
 	 private List<MessTaxInvoice> messTaxInvoice;
+	 private String billdetailsstudentname;
+	 private String billdetailsclassstudying;
+	 private String billdetailsfathername;
+	 private String billdetailstransactiondate;
+	 private String billdetailstotaltotal;
+	 private BigDecimal billgrandtotal;
+	 private String billNo;
 
 }
