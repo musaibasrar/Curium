213677f1d5428193a8db15b79024c0a49a9c6770
Mushy <musaibasrar@gmail.com>
2026-04-06 10:17:36 +0530

    Diary upload issue resolved

--- src/main/java/org/ideoholic/curium/model/diary/action/DiaryApiAction.java
+++ src/main/java/org/ideoholic/curium/model/diary/action/DiaryApiAction.java
@@ -3,20 +3,23 @@ package org.ideoholic.curium.model.diary.action;
 import org.ideoholic.curium.dto.ResultResponse;
 import org.ideoholic.curium.model.diary.dto.AddDiaryDto;
 import org.ideoholic.curium.model.diary.dto.DairyIdsDto;
-import org.ideoholic.curium.model.diary.dto.DiaryResponseDto;
 import org.ideoholic.curium.model.diary.dto.DiaryDetailsMessageResponseDto;
+import org.ideoholic.curium.model.diary.dto.DiaryResponseDto;
 import org.ideoholic.curium.model.student.dto.StudentIdDto;
 import org.ideoholic.curium.model.student.dto.StudentIdPageDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface DiaryApiAction {
 
 	 ResponseEntity<ResultResponse> getdiarystudent(@RequestHeader(value = "branchid") String branchId) ;
 
 	 ResponseEntity addDiary(@RequestBody AddDiaryDto addDiaryDto,
+			@RequestPart("fileToUpload") MultipartFile[] uploadedFiles,
 			@RequestHeader(value = "branchid") String branchId,
 			@RequestHeader(value = "currentAcademicYear") String currentAcademicYear,
 			@RequestHeader(value = "userloginid") String userLoginId);
