ffc2f4fd78d2a0e72d6d4c5056a529e22f95f3b5
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2026-04-07 09:35:24 +0530

    sibling input box is added in student form to link student with his/her siblings  and added autowired to library service
    * sibling
    
    * updated
    
    (cherry picked from commit 735126fa578e0fbf358a84051956c515b2373fea)

--- src/main/java/org/ideoholic/curium/model/student/action/StudentAction.java
+++ src/main/java/org/ideoholic/curium/model/student/action/StudentAction.java
@@ -347,6 +347,11 @@ public class StudentAction {
 		}
 	}
 	
+	@GetMapping("/searchListOfParent")
+	public void searchListOfParent() {
+				studentActionAdapter.searchListOfParent();
+  }
+  
 	@GetMapping("/checkDuplicateStudent")
 	public void checkDuplicateStudent() {
 		try {
