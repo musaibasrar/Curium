b6ffee4b21066a57b068401030f2077e19fa4cdc
Mushy <musaibasrar@gmail.com>
2026-04-08 10:05:41 +0530

    Added fees category name in fees collection details report

--- src/main/java/org/ideoholic/curium/model/feesdetails/dto/DataForFeesResponseDto.java
+++ src/main/java/org/ideoholic/curium/model/feesdetails/dto/DataForFeesResponseDto.java
@@ -3,14 +3,14 @@ package org.ideoholic.curium.model.feesdetails.dto;
 import java.util.Map;
 
 import org.ideoholic.curium.model.feescollection.dto.Receiptinfo;
-import org.ideoholic.curium.model.parents.dto.Parents;
+import org.ideoholic.curium.model.feescollection.dto.StudentFeesReport;
 
 import lombok.Data;
 
 @Data
 public class DataForFeesResponseDto {
 	
-	private Map<Receiptinfo,Parents> feesMap;
+	private Map<Receiptinfo,StudentFeesReport> feesMap;
 	private long sumOfDetailsFees;
 	private long sumOfOnlyFee;
 	private long sumOfFine;
