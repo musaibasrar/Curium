03099eecdbd4d1ba38a28be65972cdfd5c104df1
Mushy <musaibasrar@gmail.com>
2026-04-08 10:11:42 +0530

    bug fix add siblings
    (cherry picked from commit ab2eed9058c8b976a7bc2c3ddb6f8f0c9e18f7f6)

--- src/main/java/org/ideoholic/curium/model/student/action/StudentAction.java
+++ src/main/java/org/ideoholic/curium/model/student/action/StudentAction.java
@@ -348,7 +348,7 @@ public class StudentAction {
 	}
 	
 	@GetMapping("/searchListOfParent")
-	public void searchListOfParent() {
+	public void searchListOfParent() throws IOException {
 				studentActionAdapter.searchListOfParent();
   }
   
