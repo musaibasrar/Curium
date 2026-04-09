fbb32565e8beb8c2391aa4cc3f8310c60b273623
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2026-04-09 12:01:57 +0530

    apaar id and pen added in vision

--- src/main/java/org/ideoholic/curium/model/student/action/StudentActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/student/action/StudentActionAdapter.java
@@ -172,6 +172,8 @@ public class StudentActionAdapter {
         student.setOtherFeesAmount(request.getParameterValues("otherFeesFullCat"));
         student.setOtherFeesConcession(request.getParameterValues("otherFeesConcession"));
         student.setOtherTotalInstallments(request.getParameterValues("otherFeesCount"));
+        student.setPen(request.getParameter("pen"));
+        student.setApaarId(request.getParameter("apaarid"));
 
         ResultResponse resultResponse = studentService.addStudent(student, uploadedFiles, httpSession.getAttribute("branchcode").toString(), httpSession.getAttribute(Constants.BRANCHID).toString(), httpSession.getAttribute(Constants.USERID).toString(), httpSession.getAttribute(Constants.CURRENTACADEMICYEAR).toString());
 
@@ -240,6 +242,8 @@ public class StudentActionAdapter {
         studentDto.setStudentexternalid(request.getParameter("studentexternalid"));
         studentDto.setApplicationtype(request.getParameter("applicationtype"));
         studentDto.setStream(request.getParameter("stream"));
+        studentDto.setApaarId(request.getParameter("apaarid"));
+        studentDto.setPen(request.getParameter("pen"));
 
         Student student = studentService.updateStudent(uploadedFiles, studentDto, httpSession.getAttribute(Constants.BRANCHID).toString(), httpSession.getAttribute(Constants.USERID).toString(), httpSession.getAttribute(Constants.CURRENTACADEMICYEAR).toString(), httpSession.getAttribute("branchcode").toString());
 
