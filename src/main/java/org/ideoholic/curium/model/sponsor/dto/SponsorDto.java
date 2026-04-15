7a9be964fe2beee0c8c63627053022b6b1c9d7ad
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2026-04-15 10:13:16 +0530

    Sponsor feature added
    * Sponsor feature added
    
    * sponsor api added
    
    * correcting return statement
    
    * updated Sponsor respnse DTO
    
    * api changes
    
    * updated
    
    ---------
    
    Co-authored-by: musaibasrar <musaibasrar@gmail.com>
    (cherry picked from commit 45b0feeb7584722a1daad83350dde7c47192dc89)

---
+++ src/main/java/org/ideoholic/curium/model/sponsor/dto/SponsorDto.java
@@ -0,0 +1,18 @@
+package org.ideoholic.curium.model.sponsor.dto;
+
+import lombok.Data;
+
+@Data
+public class SponsorDto {
+	
+	private String id;	
+	private String name;	
+	private String contact;	
+	private String address;	
+	private String notes;	
+	private String[] sponsorIds;
+	private int userId;
+	private int branchId;
+
+
+}
