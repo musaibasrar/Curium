d5e878ef7b622d7fc8a021423d89ca657b40e85e
Mushy <musaibasrar@gmail.com>
2026-05-04 16:29:14 +0530

    removed branchid from viewDiaryStudentParent

--- src/main/java/org/ideoholic/curium/model/diary/action/DiaryApiActionImpl.java
+++ src/main/java/org/ideoholic/curium/model/diary/action/DiaryApiActionImpl.java
@@ -55,9 +55,8 @@ public class DiaryApiActionImpl implements DiaryApiAction{
 	}
 
 	@RequestMapping(value = "/viewDiaryStudentParent", method = { RequestMethod.GET, RequestMethod.POST })
-	public ResponseEntity<DiaryResponseDto> viewDiaryStudentParent(@RequestBody StudentIdPageDto studentIdPageDto,
-			@RequestHeader(value = "branchid") String branchId) {
-		DiaryResponseDto result = diaryService.viewDiaryParent(studentIdPageDto, branchId);
+	public ResponseEntity<DiaryResponseDto> viewDiaryStudentParent(@RequestBody StudentIdPageDto studentIdPageDto) {
+		DiaryResponseDto result = diaryService.viewDiaryParent(studentIdPageDto);
 		return ResponseEntity.ok(result);
 	}
 
