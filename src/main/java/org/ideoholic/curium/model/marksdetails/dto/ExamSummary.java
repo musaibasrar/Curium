19f748d416dffc7c2f7a572bb1f85637f8b90187
Mushy <musaibasrar@gmail.com>
2026-05-04 23:56:48 +0530

    Updated marks sheet for single exam

---
+++ src/main/java/org/ideoholic/curium/model/marksdetails/dto/ExamSummary.java
@@ -0,0 +1,20 @@
+package org.ideoholic.curium.model.marksdetails.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExamSummary {
+    private String examName;
+    private int examId;
+    private int totalMarks;
+    private int totalMarksObtained;
+    private double percentage;
+    private String grade;
+    private int rank;
+}
