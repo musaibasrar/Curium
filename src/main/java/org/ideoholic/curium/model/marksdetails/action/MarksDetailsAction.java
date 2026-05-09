cf996c563c4f024624aa0f8f80154f8538c11826
Mushy <musaibasrar@gmail.com>
2026-05-09 10:02:42 +0530

    minimum marks and maximum marks in marks entry

--- src/main/java/org/ideoholic/curium/model/marksdetails/action/MarksDetailsAction.java
+++ src/main/java/org/ideoholic/curium/model/marksdetails/action/MarksDetailsAction.java
@@ -1,5 +1,6 @@
 package org.ideoholic.curium.model.marksdetails.action;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 import javax.servlet.http.HttpServletRequest;
@@ -397,6 +398,16 @@ public class MarksDetailsAction {
 		    marksDetailsActionAdapter.viewMarksSub();
 			marksDetailsActionAdapter.getSubjectExams();
 		return "examMarksReportSub";
+	}
+	
+	@GetMapping("/getSubjectDetails")
+	public void getSubjectDetails() {
+		try {
+		marksDetailsActionAdapter.getSubjectDetails();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	}
 	}
 	
 }
