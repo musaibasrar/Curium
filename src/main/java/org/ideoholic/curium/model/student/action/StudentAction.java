cef38803a04b9cbc1d05497ed74e63f3b5068157
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2026-05-18 17:01:02 +0530

    Print Student Detail
    * autocomplete of student name father name etc added in student name of fee structure
    
    * print student detail in vision

--- src/main/java/org/ideoholic/curium/model/student/action/StudentAction.java
+++ src/main/java/org/ideoholic/curium/model/student/action/StudentAction.java
@@ -364,4 +364,10 @@ public class StudentAction {
 		}
 	}
 	
+	@PostMapping("/printStudentDetail")
+	public String printStudentDetail() {
+		stampFeesActionAdapter.printStudentDetail();
+		return "printstudentdetail";
+	}
+	
 }
