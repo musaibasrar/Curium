ed41b2fab092a873f55f2e39b5e38b0840bcb6c1
Mushy <musaibasrar@gmail.com>
2026-05-21 22:39:32 +0530

    Bulk Concession feature upgraded

--- src/main/java/org/ideoholic/curium/model/feescategory/action/FeesAction.java
+++ src/main/java/org/ideoholic/curium/model/feescategory/action/FeesAction.java
@@ -292,7 +292,7 @@ public class FeesAction {
 	@PostMapping("/applyBulkConcession")
 	public String applyBulkConcession() {
 		feesActionAdapter.applyBulkConcession();
-		feesCollectionActionAdapter.getFeesReport();
+		feesCollectionActionAdapter.getFeesReportDue();
 		return "bulkconcession";
 	}
 }
