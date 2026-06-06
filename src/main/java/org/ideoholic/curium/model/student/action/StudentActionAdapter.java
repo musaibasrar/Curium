a0811965b9c3e9b87d1c9c0fedf7c09b25c63576
Mushy <musaibasrar@gmail.com>
2026-06-06 11:36:22 +0530

    sibling input box is added in student form to link student with his/her siblings  and added autowired to library service

--- src/main/java/org/ideoholic/curium/model/student/action/StudentActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/student/action/StudentActionAdapter.java
@@ -13,6 +13,7 @@ import org.ideoholic.curium.model.attendance.dto.StudentAttendanceDetailsRespons
 import org.ideoholic.curium.model.feescategory.dto.StudentListResponseDto;
 import org.ideoholic.curium.model.feescollection.dto.FeesDetailsResponseDto;
 import org.ideoholic.curium.model.feescollection.dto.OtherFeesDetailsResponseDto;
+import org.ideoholic.curium.model.library.service.LibraryService;
 import org.ideoholic.curium.model.parents.dto.ParentListResponseDto;
 import org.ideoholic.curium.model.student.dto.BonafideGenerationResponseDto;
 import org.ideoholic.curium.model.student.dto.CreateStudentDto;
@@ -41,6 +42,10 @@ public class StudentActionAdapter {
 
     @Autowired
     private StudentService studentService;
+    
+    private String CURRENTACADEMICYEAR = "currentAcademicYear";
+    private String BRANCHID = "branchid";
+    private String USERID = "userloginid";
 
     public void viewAllStudentsSuperAdmin() {
 
@@ -363,6 +368,10 @@ public class StudentActionAdapter {
         httpSession.setAttribute("academicPerYear", responseDto.getAcademicPerYear());
         httpSession.setAttribute("totalfeesconcession", responseDto.getTotalFeesConcession());
     }
+
+	public void searchListOfParent() {
+		 studentService.getParentList(httpSession.getAttribute(BRANCHID).toString());
+  }
     
     public void checkDuplicateStudent()  throws IOException {
     	
