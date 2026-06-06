4da0abe896dd47d248e1e717e0ddfb4d25fc8a19
Mushy <musaibasrar@gmail.com>
2026-06-06 13:40:50 +0530

    Added exam graph in marks sheet

--- src/main/java/org/ideoholic/curium/model/marksdetails/dto/StudentGraphDto.java
+++ src/main/java/org/ideoholic/curium/model/marksdetails/dto/StudentGraphDto.java
@@ -1,9 +1,14 @@
 package org.ideoholic.curium.model.marksdetails.dto;
 
+import java.util.List;
+
+import org.ideoholic.curium.model.examdetails.dto.Exams;
+
 import lombok.Data;
 
 @Data
 public class StudentGraphDto {
     private String[] studentIds;
     private String[] examClass;
+    private List<Exams> examsList;
 }
