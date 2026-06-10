9c93175457dfafef6c2a42a8dc9cfd66e7530eb1
Mushy <musaibasrar@gmail.com>
2026-06-10 08:14:04 +0530

    Bulk Fees Concession Added

--- src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionAction.java
+++ src/main/java/org/ideoholic/curium/model/feescollection/action/FeesCollectionAction.java
@@ -334,4 +334,10 @@ public class FeesCollectionAction {
 	            feesCollectionActionAdapter.getFeesReportOutstanding();
 	            return "feesoutstanding";
 	        }
+		  
+		  @PostMapping("/searchFeesReportBulkConcession")
+	        public String searchFeesReportBulkConcession() {
+	            feesCollectionActionAdapter.getFeesReport();
+	            return "bulkconcession";
+	        }
 }
