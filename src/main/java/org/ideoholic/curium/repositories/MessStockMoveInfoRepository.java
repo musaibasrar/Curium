844a6b5c829579a881e238663c311dfa0850f43d
Mushy <musaibasrar@gmail.com>
2026-06-20 09:02:38 +0530

    1. Added total due to the issuance report 2. Added filters to due report

--- src/main/java/org/ideoholic/curium/repositories/MessStockMoveInfoRepository.java
+++ src/main/java/org/ideoholic/curium/repositories/MessStockMoveInfoRepository.java
@@ -1,10 +1,20 @@
 package org.ideoholic.curium.repositories;
 
+import java.util.List;
+
 import org.ideoholic.curium.model.mess.item.dto.MessStockMoveInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessStockMoveInfoRepository extends JpaRepository<MessStockMoveInfo, Integer> {
 	MessStockMoveInfo findTopByOrderByReceiptnumberDesc();
+	
+	@Query("SELECT m FROM MessStockMoveInfo m WHERE m.date BETWEEN :fromDate AND :toDate AND m.studentname = :studentName AND m.branchid = :branchId")
+	List<MessStockMoveInfo> findByDateRangeAndStudentAndBranch(@Param("fromDate") String fromDate, @Param("toDate") String toDate, @Param("studentName") String studentName, @Param("branchId") int branchId);
+	
+	@Query("SELECT m FROM MessStockMoveInfo m WHERE m.date BETWEEN :fromDate AND :toDate AND m.branchid = :branchId")
+	List<MessStockMoveInfo> findByDateRangeAndBranch(@Param("fromDate") String fromDate, @Param("toDate") String toDate, @Param("branchId") int branchId);
 }
