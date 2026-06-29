34c13658e8000ffa97e5d73b23afe6f84b812b05
Mushy <musaibasrar@gmail.com>
2026-06-29 21:50:01 +0530

    Fees collection details dash board bug fix for multi branch

--- src/main/java/org/ideoholic/curium/model/user/service/UserService.java
+++ src/main/java/org/ideoholic/curium/model/user/service/UserService.java
@@ -211,8 +211,8 @@ public class UserService {
 				result.setXaxisList(xaxisList);
 				result.setYaxisList(yaxisList);
 				result.setTotalStudents(totalStudents);
-				feesdailysearch();
-				feesmonthlysearch();
+				feesdailysearch(Integer.parseInt(branchId));
+				feesmonthlysearch(Integer.parseInt(branchId));
 
 				result.setSuccess(true);
 			}
@@ -223,7 +223,7 @@ public class UserService {
 		return result;
 	}
 	
-	public void feesdailysearch() {
+	public void feesdailysearch(int branchId) {
 		
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		Date newdate = new Date();
@@ -234,7 +234,7 @@ public class UserService {
 		Date dateBefore = null;
 		Date dateAfter = null;
 		
-		String queryMain = "From Receiptinfo as feesdetails where feesdetails.cancelreceipt=0 and";
+		String queryMain = "From Receiptinfo as feesdetails where feesdetails.cancelreceipt=0 and feesdetails.branchid="+branchId+" and ";
 		
 		try {
 			dateBefore = df.parse(todaysDate);
@@ -271,7 +271,7 @@ public class UserService {
 		request.setAttribute("currentdate", dailyDatesList);
 	}
 
-	public void feesmonthlysearch() {
+	public void feesmonthlysearch(int branchId) {
 		
 		List<String> monthList = new LinkedList<String>();
 		List<String> totalFeesSum = new LinkedList<String>();
@@ -281,7 +281,7 @@ public class UserService {
 		List<Receiptinfo> feesDetailsList = new ArrayList<Receiptinfo>();
 		Date dateBefore = null;
 		Date dateAfter = null;
-		String queryMain = "From Receiptinfo as feesdetails where feesdetails.cancelreceipt=0 and ";
+		String queryMain = "From Receiptinfo as feesdetails where feesdetails.cancelreceipt=0 and feesdetails.branchid="+branchId+" and  ";
 		String toDate = DataUtil.emptyString(request.getParameter("todate"));
 		String fromDate = DataUtil.emptyString(request.getParameter("fromdate"));
 		
