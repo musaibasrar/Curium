5b069ef3a8822172d78f2f7a2105c9cae50129ec
Tauqueer031 <129364394+Tauqueer031@users.noreply.github.com>
2026-07-01 13:09:16 +0530

    Quick student save added

--- src/main/java/org/ideoholic/curium/model/student/action/StudentAction.java
+++ src/main/java/org/ideoholic/curium/model/student/action/StudentAction.java
@@ -362,4 +362,10 @@ public class StudentAction {
 		return "printstudentdetail";
 	}
 	
+	@GetMapping("/QuickStudentSave")
+	public String QuickStudentSave() {
+		standardActionAdapter.viewClasses();
+		return "quickstudentsave";
+	}
+	
 }
