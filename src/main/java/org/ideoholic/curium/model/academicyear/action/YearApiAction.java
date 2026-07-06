5efa28b59812e6d8c62885292ea3baef69d9d31f
Mushy <musaibasrar@gmail.com>
2026-07-06 13:36:25 +0530

    Added startdate,enddate,branchid,userid in academic year

--- src/main/java/org/ideoholic/curium/model/academicyear/action/YearApiAction.java
+++ src/main/java/org/ideoholic/curium/model/academicyear/action/YearApiAction.java
@@ -5,10 +5,11 @@ import org.ideoholic.curium.model.academicyear.dto.CurrentAcademicYearDto;
 import org.ideoholic.curium.model.academicyear.dto.CurrentAcademicYearResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 public interface YearApiAction {
 
 	ResponseEntity<ResultResponse> saveYear(@RequestBody CurrentAcademicYearDto currentAcademicYearDto);
 	
-	ResponseEntity<CurrentAcademicYearResponseDto> updateYear();
+	ResponseEntity<CurrentAcademicYearResponseDto> updateYear(@RequestHeader(value = "branchid") String branchId);
 }
