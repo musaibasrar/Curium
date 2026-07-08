6732217b6aab84d03f8a750af80c442366b442cd
Mushy <musaibasrar@gmail.com>
2026-07-08 13:35:07 +0530

    Bonafide Certificate, GenerateDateinWord added in DateUtil

--- src/main/java/org/ideoholic/curium/model/student/action/StudentActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/student/action/StudentActionAdapter.java
@@ -85,6 +85,7 @@ public class StudentActionAdapter {
 
         BonafideGenerationResponseDto responseDto = studentService.generateBonafide(dto);
         httpSession.setAttribute("studentdetailsbonafide", responseDto.getParents());
+        request.setAttribute("dobInWords", responseDto.getDateOfBirth());
 
         return responseDto;
     }
