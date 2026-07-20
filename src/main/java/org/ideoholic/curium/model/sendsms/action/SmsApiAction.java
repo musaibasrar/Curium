500414b466409766e796fe8ffed29ef370b6e127
Mushy <musaibasrar@gmail.com>
2026-07-20 19:23:24 +0530

    bug fixes

--- src/main/java/org/ideoholic/curium/model/sendsms/action/SmsApiAction.java
+++ src/main/java/org/ideoholic/curium/model/sendsms/action/SmsApiAction.java
@@ -38,7 +38,7 @@ public interface SmsApiAction {
     public ResponseEntity<ResultResponse> sendNumbersSMS(@RequestBody  SendSMSDto dto);
     
     @GetMapping("/updateYear")
-    public ResponseEntity<CurrentAcademicYearResponseDto> updateYear();
+    public ResponseEntity<CurrentAcademicYearResponseDto> updateYear(@RequestHeader(value = Constants.BRANCHID) String branchId);
     
     @PostMapping("/sendSMSFeesDueReminder")
     public ResponseEntity<ResultResponse> sendSMSFeesDueReminder(@RequestBody SendSMSDto dto);
