9fcfff08d7ba3d51572fc35a377c22cb9937f54a
Mushy <musaibasrar@gmail.com>
2026-08-25 09:15:30 +0530

    Marks card with co scholastic subjects

--- src/main/java/org/ideoholic/curium/model/marksdetails/service/MarksDetailsService.java
+++ src/main/java/org/ideoholic/curium/model/marksdetails/service/MarksDetailsService.java
@@ -1831,6 +1831,9 @@ public GenerateReportResponseDto generateReportSingleExams(GenerateReportDto dto
 								}
 								if (!excludedGrade.isEmpty()) {
 									excludedSubjectGrades.put(subjectName, excludedGrade);
+								}else {
+									int percent = (int) ((marksObtained * 100.0) / maxMarks);
+									excludedSubjectGrades.put(subjectName, ""+percent);
 								}
 								break;
 							}
