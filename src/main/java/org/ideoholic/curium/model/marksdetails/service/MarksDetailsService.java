faba92e9451d3ea9850cb0bfd573358932346256
Mushy <musaibasrar@gmail.com>
2026-08-25 09:31:12 +0530

    update marks card print to print part-B coscholastic subjects with details

--- src/main/java/org/ideoholic/curium/model/marksdetails/service/MarksDetailsService.java
+++ src/main/java/org/ideoholic/curium/model/marksdetails/service/MarksDetailsService.java
@@ -1833,7 +1833,8 @@ public GenerateReportResponseDto generateReportSingleExams(GenerateReportDto dto
 									excludedSubjectGrades.put(subjectName, excludedGrade);
 								}else {
 									int percent = (int) ((marksObtained * 100.0) / maxMarks);
-									excludedSubjectGrades.put(subjectName, ""+percent);
+									//excludedSubjectGrades.put(subjectName, ""+percent);
+									excludedSubjectGrades.put(subjectName, marksObtained+"/"+maxMarks+"/"+percent+"/"+excludedGrade);
 								}
 								break;
 							}
