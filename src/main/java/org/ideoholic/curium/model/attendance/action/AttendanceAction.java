a58c093f8f9ffe190cc06f5cba4a50a8c515b1a4
Mushy <musaibasrar@gmail.com>
2026-09-13 12:46:01 +0530

    Change password and attendance for parent login

--- src/main/java/org/ideoholic/curium/model/attendance/action/AttendanceAction.java
+++ src/main/java/org/ideoholic/curium/model/attendance/action/AttendanceAction.java
@@ -319,4 +319,20 @@ public class AttendanceAction {
 		return "attendancesummaryreport";
 	}
 	
+	@GetMapping("/viewAttendanceParent")
+	public String viewAttendanceParent() {
+		attendanceActionAdapter.singleStudentReport();
+		return "viewAttendanceParent";
+	}
+	
+	
+	@PostMapping("/searchSingleStudentAttendanceDetailsMonthly")
+	public String searchSingleStudentAttendanceDetailsMonthly() {
+
+		if (attendanceActionAdapter.viewStudentAttendanceDetailsMonthly()) {
+			return "viewAttendanceParent";
+		}
+		return errorPage;
+	}
+	
 }
