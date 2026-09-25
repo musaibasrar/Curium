a1a04010a198ae1be29047c3010def1f87e7a3b1
Mushy <musaibasrar@gmail.com>
2026-09-25 19:34:52 +0530

    updated sendSMSFeesDueReminder method to send the selected fees category reminders

--- src/main/java/org/ideoholic/curium/model/sendsms/action/SmsActionAdapter.java
+++ src/main/java/org/ideoholic/curium/model/sendsms/action/SmsActionAdapter.java
@@ -54,6 +54,7 @@ public class SmsActionAdapter {
 
         SendSMSDto dto = new SendSMSDto();
         String[] studentIds = request.getParameterValues("studentIDs");
+        dto.setFeesIds(request.getParameterValues("feescategory"));
         dto.setStudentIds(studentIds);
         dto.setMessage(request.getParameter("deadline"));
         dto.setStudentFeesReportList((List<StudentFeesReport>) httpSession.getAttribute("studentfeesreportlist"));
